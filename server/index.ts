d3d605b12b3d17842b1359de544a9af0d73736ad
Joshua Bowers <joshua.bowers+code@gmail.com>
2021-09-05 14:54:12 -0700

    Basic server skeleton
    Server sets up logging and ability to serve client app.

---
+++ server/index.ts
@@ -0,0 +1,59 @@
+import sslRedirect from 'heroku-ssl-redirect';
+import listEndpoints from 'express-list-endpoints';
+import express, { Request, Response, NextFunction} from 'express';
+
+import helmet from 'helmet';
+import cors from 'cors';
+
+import logger from './middleware/logger';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+// import { connect } from './server/database.js';
+
+const app = express()
+// const __dirname = dirname( fileURLToPath( import.meta.url ) )
+
+app.use( sslRedirect() )
+app.use( helmet() )
+app.use( cors() )
+app.use( express.json() )
+app.use( express.urlencoded({ extended: true }) )
+
+// TODO: Replace with better logger
+app.use( logger );
+
+// import { createAuthentication } from './server/authentication.js';
+
+// createAuthentication( app );
+
+// import api from './server/api/index.js';
+
+// app.use('/api', api);
+
+// Serve static files from the React frontend app
+app.use(express.static(join(__dirname, '/client/build')))
+
+// Anything that doesn't match the above, send back index.html
+app.get('*', (req, res) => {
+  res.sendFile(join(__dirname, '/client/build/index.html'))
+})
+
+// Error handler
+app.use( (err: Error, req: Request, res: Response, next: NextFunction) => {
+  console.error(err.stack)
+  if( res.headersSent ){
+    return next( err )
+  }
+  res.status(500).json({ error: err })
+} )
+
+const PORT = process.env.PORT || 5000
+
+// connect().then(async () => {
+  app.listen(PORT, () => {
+    console.log(`Express server listening on port ${ PORT }`)
+    console.log(listEndpoints(app));
+  })
+// }).catch(async (err) => {
+//   console.error( err )
+// })
---
+++ server/middleware/logger.ts
@@ -0,0 +1,22 @@
+import chalk from 'chalk';
+import { Request, Response, NextFunction } from 'express';
+
+const timestamp = chalk.yellow;
+const method = chalk.bold.cyan;
+const uri = chalk.underline.cyan;
+const data = chalk.bold.green;
+
+export default async function logger( req: Request, res: Response, next: NextFunction ) {
+  const out = `\n${ timestamp( new Date().toUTCString() ) }: ${ method( req.method ) } ${ uri( req.originalUrl ) }`;
+  console.log( out );
+  if( Object.keys(req.headers).length ){
+    console.info( data('headers'), ':', req.headers );
+  }
+  if( Object.keys(req.query).length ){
+    console.info( data('query'), ':', req.query );
+  }
+  if( Object.keys(req.body).length ){
+    console.info( data('body'), ':', req.body );
+  }
+  next();
+}
