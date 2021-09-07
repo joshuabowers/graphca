acbf6efc44bb9d3f9885a9731c6bbe6dcf565c56
Joshua Bowers <joshua.bowers+code@gmail.com>
2021-09-07 16:37:45 -0700

    Sanity check: app working
    - Hot module reloading (maybe) working at full client app level
    - Some weirdness with Graph cleaned up

---
+++ client/src/features/Graph/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Graph = (props: void) => {
+  return (
+    <canvas />
+  )
+}
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -6,14 +6,21 @@ import { store } from './app/store';
 import { Provider } from 'react-redux';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const renderApp = () =>
+  ReactDOM.render(
+    <React.StrictMode>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+
+if(process.env.NODE_ENV !== 'production' && module.hot){
+  module.hot.accept('./app/App', renderApp);
+}
+
+renderApp();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
