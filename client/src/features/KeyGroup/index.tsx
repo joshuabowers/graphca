a36a3e34cf0419f760466cfbb88bdb976d612e6b
Joshua Bowers <joshua.bowers+code@gmail.com>
2021-09-21 19:28:15 -0700

    KeyGroup refinement
    - 'full' -> 'square', to better represent the intent
    - the side length of a KeyGroup is memoized and stored as --sideLength
    - CP used in .square to auto-flow a square layout of the keys.

--- client/src/features/KeyGroup/index.tsx
+++ client/src/features/KeyGroup/index.tsx
@@ -1,18 +1,32 @@
-import React from "react";
+import React, { useMemo, CSSProperties } from "react";
 import styles from './KeyGroup.module.css'
 
 export interface KeyGroupProps {
-  layout?: 'full' | 'horizontal' | 'vertical'
+  layout?: 'square' | 'horizontal' | 'vertical'
   children?: JSX.Element | JSX.Element[]
 }
 
+export interface KeyGroupCSSProps extends CSSProperties {
+  '--sideLength': number;
+}
+
+const isArray = (x: JSX.Element | JSX.Element[] | unknown): x is JSX.Element[] => {
+  return Array.isArray(x)
+}
+
 export const KeyGroup = (props: KeyGroupProps) => {
+  const sideLength = useMemo(
+    () => Math.ceil(Math.sqrt(isArray(props.children) ? props.children.length : 1)), 
+    [props.children]
+  )
   const appliedStyles = [
     styles.keyGroup,
-    styles[props.layout || 'full']
+    styles[props.layout || 'square']
   ];
   return (
-    <div className={appliedStyles.join(' ')}>
+    <div 
+      className={appliedStyles.join(' ')} 
+      style={{'--sideLength': sideLength} as KeyGroupCSSProps}>
       {props.children}
     </div>
   )
