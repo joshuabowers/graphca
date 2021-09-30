9447fb35a7d97a678073056d8eb5c93df12bd933
Joshua Bowers <joshua.bowers+code@gmail.com>
2021-09-29 18:45:44 -0700

    Pegase grammar go brrr...
    - PEG has been expanded to automatically generate an AST, per pegase
    - Currently supports:
    -- Plus
    -- Minus
    -- Multiply
    -- Divide
    -- Exponent
    -- Negate
    -- Number
    -- Variable
    -- function invocations: sin, cos, tan, lg, ln, log
    - Should be exhibiting proper operator precedence and associativity.

--- client/src/common/parser.spec.ts
+++ client/src/common/parser.spec.ts
@@ -1,29 +1,124 @@
 import { parser } from "./parser";
 
+// Helper functions for generating tree nodes to check.
+const num = (val: string) => ({'$label': 'NUMBER', 'value': val})
+const variable = (name: string) => ({'$label': 'VARIABLE', 'name': name})
+
 describe('parser', () => {
   it('matches numbers', () => {
     const inputs = '1 10 0 0.25 10.25 1.23E4 1.23E-4'.split(' ');
     inputs.forEach(input => {
       expect(parser.parse(input).logger.print()).toEqual('')
-      expect(parser.children(input)).toEqual([input])
+      expect(parser.value(input)).toMatchObject(num(input))
     })
   })
 
   it('matches variables', () => {
-    expect(parser.test('x')).toBeTruthy()
-    expect(parser.test('zed')).toBeTruthy()
-    expect(parser.children('x')).toEqual(['x'])
-    expect(parser.children('zed')).toEqual(['zed'])
+    const variables = ['x', 'zed', 'y10']
+    variables.forEach(v => {
+      expect(parser.test(v)).toBeTruthy()
+      expect(parser.value(v)).toMatchObject(variable(v))
+    })
+    expect(parser.test('10x')).toBeFalsy()
+    expect(parser.test('tan1')).toBeFalsy()
   })
 
   it('matches basic arithmetic', () => {
-    expect(parser.test('x + 1')).toBeTruthy()
-    expect(parser.children('x + 1')).toEqual(['x', '+', '1'])
-    // TODO: Revisit
-    expect(parser.children('1 + x * 2')).toEqual(['1', '+', 'x', '*', '2'])
+    expect(parser.value('x + 1')).toMatchObject({
+      '$label': 'PLUS', 
+      'a': variable('x'), 
+      'b': num('1')
+    })
+    expect(parser.value('1 + x * 2')).toMatchObject({
+      '$label': 'PLUS',
+      'a': num('1'),
+      'b': {
+        '$label': 'MULTIPLY',
+        'a': variable('x'),
+        'b': num('2')
+      }
+    })
+  })
+
+  it('handles operator precedence and associativity', () => {
+    expect(parser.value('1 + 2 * 3 - 4 / n')).toMatchObject({
+      '$label': 'PLUS',
+      'a': num('1'),
+      'b': {
+        '$label': 'MINUS',
+        'a': {
+          '$label': 'MULTIPLY',
+          'a': num('2'),
+          'b': num('3')
+        },
+        'b': {
+          '$label': 'DIVIDE',
+          'a': num('4'),
+          'b': variable('n')
+        }
+      }
+    })
+  })
+
+  it('matches a function call', () => {
+    expect(parser.value('cos(x)')).toMatchObject({
+      '$label': 'COS',
+      'args': variable('x')
+    })
   })
 
   it('matches functional expressions', () => {
-    expect(parser.children('cos(lg(1024*x))')).toEqual(['cos', 'lg', '1024', '*', 'x'])
+    expect(parser.value('cos(lg(1024*x))')).toMatchObject({
+      '$label': 'COS',
+      'args': {
+        '$label': 'LG',
+        'args': {
+          '$label': 'MULTIPLY',
+          'a': num('1024'),
+          'b': variable('x')
+        }
+      }
+    })
+  })
+
+  it('matches functional expressions in arithmetic', () => {
+    expect(parser.value('1 + 2 * cos(x)')).toMatchObject({
+      '$label': 'PLUS',
+      'a': num('1'),
+      'b': {
+        '$label': 'MULTIPLY',
+        'a': num('2'),
+        'b': {
+          '$label': 'COS',
+          'args': variable('x')
+        }
+      }
+    })
+  })
+
+  it('matches negations', () => {
+    expect(parser.value('-1')).toMatchObject({
+      '$label': 'NEGATE',
+      'expression': num('1')
+    })
+    expect(parser.value('-(x - 1)')).toMatchObject({
+      '$label': 'NEGATE',
+      'expression': {
+        '$label': 'MINUS',
+        'a': variable('x'),
+        'b': num('1')
+      }
+    })
+  })
+
+  it('matches exponents', () => {
+    expect(parser.value('(-x)^2')).toMatchObject({
+      '$label': 'EXPONENT',
+      'base': {
+        '$label': 'NEGATE',
+        'expression': variable('x')
+      },
+      'power': num('2')
+    })
   })
 })
--- client/src/common/parser.ts
+++ client/src/common/parser.ts
@@ -1,15 +1,44 @@
-import peg from 'pegase';
+import peg, { $node } from 'pegase';
 
-export const expression = peg`
-expression: functional | arithmetic
-arithmetic: term % ("+" | "-")
-term: exponent % ("*" | "/")
-exponent: factor ("^" expression)?
-factor: number | variable | '(' expression ')' | '-' expression
-functional: callable '(' expression ')'
+export const parser = peg`
+expression: arithmetic
+
+arithmetic:
+| <a>product '+' ^ <b>arithmetic => 'PLUS'
+| <a>product '-' ^ <b>arithmetic => 'MINUS'
+| product
+
+product:
+| <a>exponent '*' ^ <b>product => 'MULTIPLY'
+| <a>exponent '/' ^ <b>product => 'DIVIDE'
+| exponent
+
+exponent:
+| <base>negation '^' ^ <power>expression => 'EXPONENT'
+| negation
+
+negation:
+| '-' ^ <expression>grouping => 'NEGATE'
+| grouping
+
+grouping:
+| functional
+| '(' ^ expression ')'
+| factor
+
+factor:
+| <value>number => 'NUMBER'
+| <name>variable => 'VARIABLE'
+
+functional: <f>callable '(' <args>expression ')' ${
+({f, args}) => $node((<string>f).toLocaleUpperCase(), {args})
+}
+
+keywords: callable
+
 callable: "sin" | "cos" | "tan" | "lg" | "ln" | "log"
-$number @raw: integer ('.' integer)? ('E' '-'? integer)?
+
+$number @raw: (integer ('.' integer)? ('E' '-'? integer)?)
 $integer @raw: 0 | [1-9][0-9]*
-$variable @raw: [a-zA-Z][a-zA-Z0-9]*
-`;
-export const parser = peg`${expression}`;
+$variable @raw: !(keywords) [a-zA-Z][a-zA-Z0-9]*
+`
