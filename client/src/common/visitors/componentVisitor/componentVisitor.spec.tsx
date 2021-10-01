33ca0f2bc0b7884625483ec4bd98d524abe58e1e
Joshua Bowers <joshua.bowers+code@gmail.com>
2021-10-01 16:24:45 -0700

    componentVisitor handles current AST
    All currently extant nodes which get generated by the parser
    should now be visitable via componentVisitor. The visitor
    builds simple, nested components, where each node generates a
    wrapping <span> with an appropriate className. Styling to follow.

--- client/src/common/visitors/componentVisitor/componentVisitor.spec.tsx
+++ client/src/common/visitors/componentVisitor/componentVisitor.spec.tsx
@@ -5,19 +5,57 @@ import { MathSymbols, Unicode } from '../../MathSymbols';
 import { parser } from '../../parser';
 import { componentVisitor } from '.';
 
+const apply = (input: string) => parser.value(input, {visit: componentVisitor})
+const expectMarkup = (input: string, className: string, expected?: string) => {
+  const output = apply(input);
+  expect(shallow(output).is(className)).toBe(true);
+  expect(shallow(output).text()).toEqual(expected ?? input);
+}
+
 describe('componentVisitor', () => {
   it('renders numbers', () => {
-    const input = '1024';
-    const output = parser.value(input, {visit: componentVisitor});
-    expect(shallow(output).is('.number')).toBe(true);
-    expect(shallow(output).text()).toEqual('1024');
+    expectMarkup('1024', '.number')
   })
 
   it('renders binary operator expressions', () => {
-    const input = '1 + 2 - 3 * 4 / 5';
-    const output = parser.value(input, {visit: componentVisitor});
-    const expected = `1 + 2 ${Unicode.minus} 3 ${Unicode.multiplication} 4 ${Unicode.division} 5`
-    expect(shallow(output).is('.binaryOp')).toBe(true)
-    expect(shallow(output).text()).toEqual(expected)
+    expectMarkup('1 + 2', '.binaryOp')
+    expectMarkup('2 - 3', '.binaryOp', `2 ${Unicode.minus} 3`)
+    expectMarkup('3 * 4', '.binaryOp', `3 ${Unicode.multiplication} 4`)
+    expectMarkup('4 / 5', '.binaryOp', `4 ${Unicode.division} 5`)
+    expectMarkup(
+      '1 + 2 - 3 * 4 / 5', 
+      '.binaryOp', 
+      `1 + 2 ${Unicode.minus} 3 ${Unicode.multiplication} 4 ${Unicode.division} 5`
+    )
+  })
+
+  it('renders variables', () => {
+    expectMarkup('x', '.variable')
+  })
+
+  it('renders negations', () => {
+    expectMarkup('-x', '.negation', `${Unicode.minus}x`)
+    expectMarkup('-1', '.negation', `${Unicode.minus}1`)
+  })
+
+  it('renders exponents', () => {
+    expectMarkup('x^2', '.exponent')
+  })
+
+  it('renders trigonometric functions', () => {
+    expectMarkup('cos(x)', '.functional.trigonometric')
+    expectMarkup('sin(x)', '.functional.trigonometric')
+    expectMarkup('tan(x)', '.functional.trigonometric')
+  })
+
+  it('renders logarithmic functions', () => {
+    expectMarkup('lg(x)', '.functional.logarithmic')
+    expectMarkup('ln(x)', '.functional.logarithmic')
+    expectMarkup('log(x)', '.functional.logarithmic')
+  })
+
+  it('renders nested functions', () => {
+    expectMarkup('cos(log(x))', '.functional.trigonometric')
+    expectMarkup('log(cos(x))', '.functional.logarithmic')
   })
 })
--- client/src/common/visitors/componentVisitor/index.tsx
+++ client/src/common/visitors/componentVisitor/index.tsx
@@ -9,10 +9,29 @@ const binaryOp = (node: Node, op: MathSymbols) => (
   </span>
 )
 
+const functional = (node: Node, metaClass: string) => (
+  <span className={[styles.functional, styles[metaClass]].join(' ')}>
+    {node.$label.toLocaleLowerCase()}({$visit(node.args)})
+  </span>
+)
+
 export const componentVisitor: Visitor<JSX.Element> = {
   NUMBER: (node) => <span className={styles.number}>{node.value}</span>,
+  VARIABLE: (node) => <span className={styles.variable}>{node.name}</span>,
   PLUS: (node) => binaryOp(node, '+'),
   MINUS: (node) => binaryOp(node, Unicode.minus),
   MULTIPLY: (node) => binaryOp(node, Unicode.multiplication),
   DIVIDE: (node) => binaryOp(node, Unicode.division),
+  EXPONENT: (node) => (
+    <span className={styles.exponent}>
+      {$visit(node.base)}^{$visit(node.power)}
+    </span>
+  ),
+  NEGATE: (node) => <span className={styles.negation}>{Unicode.minus}{$visit(node.expression)}</span>,
+  SIN: (node) => functional(node, 'trigonometric'),
+  COS: (node) => functional(node, 'trigonometric'),
+  TAN: (node) => functional(node, 'trigonometric'),
+  LG: (node) => functional(node, 'logarithmic'),
+  LN: (node) => functional(node, 'logarithmic'),
+  LOG: (node) => functional(node, 'logarithmic')
 }
