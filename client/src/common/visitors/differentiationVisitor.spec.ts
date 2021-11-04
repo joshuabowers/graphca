dfadeac8000a40b48fd0670a601069105a9f6cf8
Joshua Bowers <joshua.bowers+code@gmail.com>
2021-11-04 16:27:37 -0700

    Developing derivatives
    Additions, Subtractions, Multiplications, and Divisions should be
    functional. Variables either return the constant 1, or the derivative
    of an associated value.

--- client/src/common/visitors/differentiationVisitor.spec.ts
+++ client/src/common/visitors/differentiationVisitor.spec.ts
@@ -10,21 +10,86 @@ const apply = (input: string, scope: Scope) => parser.value(
   input, {visit: [evaluateVisitor, differentiationVisitor], context: scope}
 )
 
-const expectObject = (input: string, expected: object) => {
+const parse = (input: string) => parser.value(
+  input, {visit: evaluateVisitor}
+)
+
+type ScopeEntry = {[x: string]: Node}
+
+const expectObject = (input: string, scopeEntries: ScopeEntry, expected: object) => {
   let output = undefined
   const scope = new Scope()
+  Object.entries(scopeEntries).forEach(([key, value]) => {
+    scope.set(key, value)
+  })
   expect(() => {output = apply(input, scope)}).not.toThrow()
   expect(output).not.toBeUndefined()
   expect(output).toMatchObject(expected)
 }
 
+type NodeLike = Omit<Node, '$from' | '$to'>
+
+const binary = ($label: string, a: NodeLike, b: NodeLike) => ({
+  $label, a, b
+})
+
 const real = (val: string) => ({'$label': 'REAL', 'value': new Real(val)})
+const variable = (name: string) => ({'$label': 'VARIABLE', name})
+
+const add = (a: NodeLike, b: NodeLike) => binary('PLUS', a, b)
+const subtract = (a: NodeLike, b: NodeLike) => binary('MINUS', a, b)
+const multiply = (a: NodeLike, b: NodeLike) => binary('MULTIPLY', a, b)
+const divide = (a: NodeLike, b: NodeLike) => binary('DIVIDE', a, b)
+const raise = (a: NodeLike, b: NodeLike) => binary('EXPONENT', a, b)
 
 describe('differentiationVisitor', () => {
   describe('of constants', () => {
     it('returns 0 for any constant', () => {
-      expectObject('5', real('0'))
-      expectObject(Unicode.infinity, real('0'))
+      expectObject('5', {}, real('0'))
+      expectObject(Unicode.infinity, {}, real('0'))
+    })
+  })
+
+  describe('of variables', () => {
+    it('returns 1 if the variable is undefined', () => {
+      expectObject('x', {}, real('1'))
+    })
+
+    it('returns the derivative of variable if it is defined', () => {
+      expectObject('x', {'x': parse('5')}, real('0'))
+    })
+  })
+
+  describe('of additions', () => {
+    it('returns the sum of the derivatives of the parts', () => {
+      expectObject('x + 5', {}, add(real('1'), real('0')))
+    })
+  })
+
+  describe('of subtractions', () => {
+    it('returns the difference of the derivatives of the parts', () => {
+      expectObject('x - 5', {}, subtract(real('1'), real('0')))
+    })
+  })
+
+  describe('of multiplications', () => {
+    it('returns the product rule of the multiplicands', () => {
+      expectObject('5 * x', {}, add(
+        multiply(real('0'), variable('x')), 
+        multiply(real('5'), real('1'))
+      ))
+    })
+  })
+
+  describe('of divisions', () => {
+    it('returns the quotient rule of the parts', () => {
+      expectObject('5 / x', {}, divide(
+        subtract(
+          multiply(real('0'), variable('x')),
+          multiply(real('1'), real('5'))
+        ),
+        raise(variable('x'), real('2'))
+      ))
     })
   })
 })
--- client/src/common/visitors/differentiationVisitor.ts
+++ client/src/common/visitors/differentiationVisitor.ts
@@ -1,6 +1,55 @@
 import { Visitor, Node, $visit, $node } from 'pegase'
 import { Real } from '../fields/Real'
 
+const binaryOp = (node: Node): Node => {
+  const a = $visit(node.a), b = $visit(node.b)
+  return $node(node.$label, {a, b})
+}
+
 export const differentiationVisitor: Visitor<Node> = {
-  REAL: (node) => $node('REAL', {value: Real.Zero})
+  REAL: (node) => $node('REAL', {value: Real.Zero}),
+  VARIABLE: (node) => $node('REAL', {value: new Real(1)}),
+
+  PLUS: binaryOp,
+  MINUS: binaryOp,
+
+  /**
+   * Applies the product rule to the node:
+   * for a node representing f * g,
+   * (f * g)' === (f' * g) + (f * g')
+   * @param node a multiplication node to differentiate
+   * @returns the product rule
+   */
+  MULTIPLY: (node) => {
+    const a = $visit(node.a), b = $visit(node.b)
+    return $node('PLUS', {
+      a: $node('MULTIPLY', {
+        a, b: node.b
+      }),
+      b: $node('MULTIPLY', {
+        a: node.a, b
+      })
+    })
+  },
+
+  /**
+   * Applies the quotient rule to the node:
+   * for a node representing f / g,
+   * (f / g)' === ((f' * g) - (g' * f)) / g ** 2
+   * @param node a division node to differentiate
+   * @returns the quotient rule
+   */
+  DIVIDE: (node) => {
+    const a = $visit(node.a), b = $visit(node.b)
+    return $node('DIVIDE', {
+      a: $node('MINUS', {
+        a: $node('MULTIPLY', {a, b: node.b}),
+        b: $node('MULTIPLY', {a: b, b: node.a})
+      }),
+      b: $node('EXPONENT', {
+        a: node.b, 
+        b: $node('REAL', {value: new Real(2)
+      })})
+    })
+  }
 }
