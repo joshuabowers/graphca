f975469f3cecf1c765fcd13ff3ae647daeeac6a8
Joshua Bowers <joshua.bowers+code@gmail.com>
2021-11-05 13:57:21 -0700

    Extracted node construction to wrapper functions
    This ensures that the fields are consistently set. Also cuts down on
    required keypresses and bytes to represent an new subtree.

--- client/src/common/visitors/differentiationVisitor.ts
+++ client/src/common/visitors/differentiationVisitor.ts
@@ -1,21 +1,26 @@
 import { Visitor, Node, $visit, $node } from 'pegase'
 import { Real } from '../fields/Real'
 
-const binaryOp = (node: Node): Node => {
-  const a = $visit(node.a), b = $visit(node.b)
-  return $node(node.$label, {a, b})
-}
+const unary = (label: string, expression: Node) => $node(label, {expression})
 
-const real = (value: number): Node => {
-  return $node('REAL', {value: new Real(value)})
-}
+const real = (value: number) => $node('REAL', {value: new Real(value)})
+const add = (a: Node, b: Node) => $node('PLUS', {a, b})
+const subtract = (a: Node, b: Node) => $node('MINUS', {a, b})
+const multiply = (a: Node, b: Node) => $node('MULTIPLY', {a, b})
+const divide = (a: Node, b: Node) => $node('DIVIDE', {a, b})
+const raise = (a: Node, b: Node) => $node('EXPONENT', {a, b})
+const negate = (expression: Node) => unary('NEGATE', expression)
+const cos = (expression: Node) => unary('COS', expression)
+const sin = (expression: Node) => unary('SIN', expression)
+const tan = (expression: Node) => unary('TAN', expression)
+const ln = (expression: Node) => unary('LN', expression)
 
 export const differentiationVisitor: Visitor<Node> = {
   REAL: (node) => real(0),
   VARIABLE: (node) => real(1),
 
-  PLUS: binaryOp,
-  MINUS: binaryOp,
+  PLUS: (node) => add($visit(node.a), $visit(node.b)),
+  MINUS: (node) => subtract($visit(node.a), $visit(node.b)),
 
   /**
    * Applies the product rule to the node:
@@ -26,14 +31,10 @@ export const differentiationVisitor: Visitor<Node> = {
    */
   MULTIPLY: (node) => {
     const a = $visit(node.a), b = $visit(node.b)
-    return $node('PLUS', {
-      a: $node('MULTIPLY', {
-        a, b: node.b
-      }),
-      b: $node('MULTIPLY', {
-        a: node.a, b
-      })
-    })
+    return add(
+      multiply(a, node.b),
+      multiply(node.a, b)
+    )
   },
 
   /**
@@ -45,16 +46,13 @@ export const differentiationVisitor: Visitor<Node> = {
    */
   DIVIDE: (node) => {
     const a = $visit(node.a), b = $visit(node.b)
-    return $node('DIVIDE', {
-      a: $node('MINUS', {
-        a: $node('MULTIPLY', {a, b: node.b}),
-        b: $node('MULTIPLY', {a: b, b: node.a})
-      }),
-      b: $node('EXPONENT', {
-        a: node.b, 
-        b: real(2)
-      })
-    })
+    return divide(
+      subtract(
+        multiply(a, node.b),
+        multiply(b, node.a)
+      ),
+      raise(node.b, real(2))
+    )
   },
 
   /**
@@ -66,19 +64,19 @@ export const differentiationVisitor: Visitor<Node> = {
    */
   EXPONENT: (node) => {
     const a = $visit(node.a), b = $visit(node.b)
-    return $node('MULTIPLY', {
-      a: node,
-      b: $node('PLUS', {
-        a: $node('MULTIPLY', {
-          a: a,
-          b: $node('DIVIDE', {a: node.b, b: node.a})
-        }),
-        b: $node('MULTIPLY', {
-          a: b,
-          b: $node('LN', {expression: node.a})
-        })
-      })
-    })
+    return multiply(
+      node,
+      add(
+        multiply(
+          a,
+          divide(node.b, node.a)
+        ),
+        multiply(
+          b,
+          ln(node.a)
+        )
+      )
+    )
   },
 
   /**
@@ -88,82 +86,76 @@ export const differentiationVisitor: Visitor<Node> = {
    * @param node a negation to differentiate
    * @returns the negation of the derivative of the inner expression
    */
-  NEGATE: (node) => $node('NEGATE', {expression: $visit(node.expression)}),
+  NEGATE: (node) => negate($visit(node.expression)),
 
   COS: (node) => {
     const expression = $visit(node.expression)
-    return $node('MULTIPLY', {
-      a: $node('NEGATE', {expression: $node('SIN', {expression: node.expression})}),
-      b: expression
-    })
+    return multiply(
+      negate(sin(node.expression)),
+      expression
+    )
   },
 
   SIN: (node) => {
     const expression = $visit(node.expression)
-    return $node('MULTIPLY', {
-      a: $node('COS', {expression: node.expression}),
-      b: expression
-    })
+    return multiply(
+      cos(node.expression),
+      expression
+    )
   },
 
   TAN: (node) => {
     const expression = $visit(node.expression)
-    return $node('MULTIPLY', {
-      a: $node('PLUS', {
-        a: real(1),
-        b: $node('EXPONENT', {
-          a: $node('TAN', {expression: node.expression}),
-          b: real(2)
-        })
-      }),
-      b: expression
-    })
+    return multiply(
+      add(
+        real(1),
+        raise(
+          tan(node.expression),
+          real(2)
+        )
+      ),
+      expression
+    )
   },
 
   ACOS: (node) => {
     const expression = $visit(node.expression)
-    return $node('NEGATE', {
-      expression: $node('DIVIDE', {
-        a: expression,
-        b: $node('EXPONENT', {
-          a: $node('MINUS', {
-            a: real(1),
-            b: $node('EXPONENT', {a: node.expression, b: real(2)})
-          }),
-          b: real(0.5)
-        })
-      })
-    })
+    return negate(
+      divide(
+        expression,
+        raise(
+          subtract(
+            real(1),
+            raise(node.expression, real(2))
+          ),
+          real(0.5)
+        )
+      )
+    )
   },
 
   ASIN: (node) => {
     const expression = $visit(node.expression)
-    return $node('DIVIDE', {
-      a: expression,
-      b: $node('EXPONENT', {
-        a: $node('MINUS', {
-          a: real(1),
-          b: $node('EXPONENT', {
-            a: node.expression,
-            b: real(2)
-          })
-        }),
-        b: real(0.5)
-      })
-    })
+    return divide(
+      expression,
+      raise(
+        subtract(
+          real(1),
+          raise(node.expression, real(2))
+        ),
+        real(0.5)
+      )
+    )
   },
 
   ATAN: (node) => {
     const expression = $visit(node.expression)
-    return $node('DIVIDE', {
-      a: expression,
-      b: $node('PLUS', {
-        a: real(1),
-        b: $node('EXPONENT', {
-          a: node.expression,
-          b: real(2)
-        })
-      })
-    })
+    return divide(
+      expression,
+      add(
+        real(1),
+        raise(node.expression, real(2))
+      )
+    )
   }
 }
