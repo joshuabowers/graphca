5959d0f8a9f8707605d59f7094a0a25ab16c791d
Joshua Bowers <joshua.bowers+code@gmail.com>
2021-11-22 16:17:32 -0800

    Added in extra subclasses for functional approach

--- client/src/common/Tree/Expression.ts
+++ client/src/common/Tree/Expression.ts
@@ -34,9 +34,6 @@ export class Variable extends Base {
   constructor(readonly name: string) { super() }
 }
 
-// export type BinaryFunction =
-//   | Kind.Addition | Kind.Multiplication | Kind.Exponentiation
-
 export abstract class Binary extends Base {
   constructor(readonly left: Base, readonly right: Base) { super() }
 }
@@ -53,37 +50,158 @@ export class Exponentiation extends Binary {
   readonly $kind = 'Exponentiation'
 }
 
+export class Logarithm extends Binary {
+  readonly $kind = 'Logarithm'
+}
+
+export class Polygamma extends Binary {
+  readonly $kind = 'Polygamma'
+}
+
+export abstract class Unary extends Base {
+  constructor(readonly expression: Base) { super() }
+}
+
+export class AbsoluteValue extends Unary {
+  readonly $kind = 'AbsoluteValue'
+}
+
+export class Factorial extends Unary {
+  readonly $kind = 'Factorial'
+}
+
+export class Gamma extends Unary {
+  readonly $kind = 'Gamma'
+}
+
+export abstract class Trigonometric extends Unary {}
+
+export class Cosine extends Trigonometric {
+  readonly $kind = 'Cosine'
+}
+
+export class Sine extends Trigonometric {
+  readonly $kind = 'Sine'
+}
+
+export class Tangent extends Trigonometric {
+  readonly $kind = 'Tangent'
+}
+
+export class Secant extends Trigonometric {
+  readonly $kind = 'Secant'
+}
+
+export class Cosecant extends Trigonometric {
+  readonly $kind = 'Cosecant'
+}
+
+export class Cotangent extends Trigonometric {
+  readonly $kind = 'Cotangent'
+}
+
+export abstract class Arcus extends Unary {}
+
+export class ArcusCosine extends Arcus {
+  readonly $kind = 'ArcusCosine'
+}
+
+export class ArcusSine extends Arcus {
+  readonly $kind = 'ArcusSine'
+}
+
+export class ArcusTangent extends Arcus {
+  readonly $kind = 'ArcusTangent'
+}
+
+export class ArcusSecant extends Arcus {
+  readonly $kind = 'ArcusSecant'
+}
+
+export class ArcusCosecant extends Arcus {
+  readonly $kind = 'ArcusCosecant'
+}
+
+export class ArcusCotangent extends Arcus {
+  readonly $kind = 'ArcusCotangent'
+}
+
+export abstract class Hyperbolic extends Unary {}
+
+export class HyperbolicCosine extends Hyperbolic {
+  readonly $kind = 'HyperbolicCosine'
+}
+
+export class HyperbolicSine extends Hyperbolic {
+  readonly $kind = 'HyperbolicSine'
+}
+
+export class HyperbolicTangent extends Hyperbolic {
+  readonly $kind = 'HyperbolicTangent'
+}
+
+export class HyperbolicSecant extends Hyperbolic {
+  readonly $kind = 'HyperbolicSecant'
+}
+
+export class HyperbolicCosecant extends Hyperbolic {
+  readonly $kind = 'HyperbolicCosecant'
+}
+
+export class HyperbolicCotangent extends Hyperbolic {
+  readonly $kind = 'HyperbolicCotangent'
+}
+
+export abstract class AreaHyperbolic extends Unary {}
+
+export class AreaHyperbolicCosine extends AreaHyperbolic {
+  readonly $kind = 'AreaHyperbolicCosine'
+}
+
+export class AreaHyperbolicSine extends AreaHyperbolic {
+  readonly $kind = 'AreaHyperbolicSine'
+}
+
+export class AreaHyperbolicTangent extends AreaHyperbolic {
+  readonly $kind = 'AreaHyperbolicTangent'
+}
+
+export class AreaHyperbolicSecant extends AreaHyperbolic {
+  readonly $kind = 'AreaHyperbolicSecant'
+}
+
+export class AreaHyperbolicCosecant extends AreaHyperbolic {
+  readonly $kind = 'AreaHyperbolicCosecant'
+}
+
+export class AreaHyperbolicCotangent extends AreaHyperbolic {
+  readonly $kind = 'AreaHyperbolicCotangent'
+}
+
+export class Derivative extends Base {
+  readonly $kind = 'Derivative'
+  constructor(readonly expression: Base, readonly order: Base, readonly wrt: Variable[]) { super() }
+}
+
 export type Tree = 
   | Real | Complex | Variable 
   | Addition | Multiplication | Exponentiation
 
-// export interface Addition extends Binary {
-//   $kind: Kind.Addition
+// export interface Unary {
+//   $kind: 
+//     | 'AbsoluteValue' | 'Logarithm' | 'Factorial' | 'Gamma'
+//     | 'ArcusCosecant' | 'ArcusCosine' | 'ArcusCotangent' 
+//     | 'ArcusSecant' | 'ArcusSine'| 'ArcusTangent'  
+//     | 'AreaHyperbolicCosecant' | 'AreaHyperbolicCosine'
+//     | 'AreaHyperbolicCotangent' | 'AreaHyperbolicSecant'
+//     | 'AreaHyperbolicSine' | 'AreaHyperbolicTangent'
+//     | 'HyperbolicCosecant' | 'HyperbolicCosine'
+//     | 'HyperbolicCotangent' | 'HyperbolicSecant'
+//     | 'HyperbolicSine' | 'HyperbolicTangent'
+//     | 'Sine' | 'Cosine' | 'Tangent'
+//     | 'Cosecant' | 'Secant' | 'Cotangent'
+//   expression: FunExp
 // }
-
-// export interface Multiplication extends Binary {
-//   $kind: Kind.Multiplication
-// }
-
-// export interface Exponentiation extends Binary {
-//   $kind: Kind.Exponentiation
-// }
-
-export interface Unary {
-  $kind: 
-    | 'AbsoluteValue' | 'Logarithm' | 'Factorial' | 'Gamma'
-    | 'ArcusCosecant' | 'ArcusCosine' | 'ArcusCotangent' 
-    | 'ArcusSecant' | 'ArcusSine'| 'ArcusTangent'  
-    | 'AreaHyperbolicCosecant' | 'AreaHyperbolicCosine'
-    | 'AreaHyperbolicCotangent' | 'AreaHyperbolicSecant'
-    | 'AreaHyperbolicSine' | 'AreaHyperbolicTangent'
-    | 'HyperbolicCosecant' | 'HyperbolicCosine'
-    | 'HyperbolicCotangent' | 'HyperbolicSecant'
-    | 'HyperbolicSine' | 'HyperbolicTangent'
-    | 'Sine' | 'Cosine' | 'Tangent'
-    | 'Cosecant' | 'Secant' | 'Cotangent'
-  expression: FunExp
-}
 
 // export interface Polygamma {
 //   $kind: Kind.Polygamma
