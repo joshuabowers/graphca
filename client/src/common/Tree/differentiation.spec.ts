286219c133427aa7a3e99c2f1b41d19bcc0f481f
Joshua Bowers <joshua.bowers+code@gmail.com>
2021-11-24 17:14:03 -0800

    Differentiation further expanded
    Logarithmic, trigonometric, arcus, absolute value.

--- client/src/common/Tree/differentiation.spec.ts
+++ client/src/common/Tree/differentiation.spec.ts
@@ -1,10 +1,13 @@
 import { real } from "./real";
 import { complex } from "./complex";
 import { variable } from "./var";
-import { add } from './addition';
-import { multiply } from "./multiplication";
-import { raise, square } from './exponentiation';
-import { ln } from "./logarithmic";
+import { add, subtract } from './addition';
+import { multiply, divide, negate } from "./multiplication";
+import { raise, square, sqrt, reciprocal } from './exponentiation';
+import { lb, ln, lg } from "./logarithmic";
+import { abs } from "./absolute";
+import { cos, sin, tan, sec, csc, cot } from './trigonometric';
+import { acos, asin, atan, asec, acsc, acot } from './arcus';
 
 import { differentiate } from "./differentiation";
 
@@ -31,11 +34,28 @@ describe('differentiate', () => {
     it('is the sum of the derivatives', () => {
       expect(differentiate(add(variable('x'), real(1)))).toEqual(real(1))
     })
+
+    it('handles subtractions correctly', () => {
+      expect(differentiate(subtract(real(1), variable('x')))).toEqual(real(-1))
+    })
   })
 
   describe('of multiplications', () => {
     it('is the sum of the products of the derivatives of the parts', () => {
       expect(differentiate(multiply(real(2), variable('x')))).toEqual(real(2))
+    })
+
+    it('handles divisions correctly', () => {
+      expect(differentiate(divide(real(5), variable('x')))).toEqual(
+        divide(
+          real(5),
+          square(variable('x'))
+        )
+      )
+    })
+
+    it('returns the negation of derivative of the expression', () => {
+      expect(differentiate(negate(variable('x')))).toEqual(real(-1))
     })
   })
 
@@ -54,5 +74,132 @@ describe('differentiate', () => {
         )
       )
     })
+  })
+
+  describe('of absolute values', () => {
+    it('returns the chain rule of the derivative of the absolute', () => {
+      expect(differentiate(abs(variable('x')))).toEqual(
+        divide(variable('x'), abs(variable('x')))
+      )
+    })
+  })
+
+  describe('of logarithms', () => {
+    it('is a chained derivative of the argument and a binary logarithm', () => {
+      expect(differentiate(lb(variable('x')))).toEqual(
+        divide(
+          real(1),
+          multiply(variable('x'), ln(real(2)))
+        )
+      )
+    })
+
+    it('is a chained derivative of the argument and a natural logarithm', () => {
+      expect(differentiate(ln(variable('x')))).toEqual(
+        divide(
+          real(1),
+          variable('x')
+        )
+      )
+    })
+
+    it('is a chained derivative of the argument and a common logarithm', () => {
+      expect(differentiate(lg(variable('x')))).toEqual(
+        divide(
+          real(1),
+          multiply(variable('x'), ln(real(10)))
+        )
+      )
+    })
+  })
+
+  describe('of trigonometric functions', () => {
+    it('returns the chain rule of the derivative of cosine of an expression', () => {
+      expect(differentiate(cos(variable('x')))).toEqual(
+        negate(sin(variable('x')))
+      )
+    })
+
+    it('returns the chain rule of the derivative of sine of an expression', () => {
+      expect(differentiate(sin(variable('x')))).toEqual(
+        cos(variable('x'))
+      )
+    })
+
+    it('returns the chain rule of the derivative of tangent of an expression', () => {
+      expect(differentiate(tan(variable('x')))).toEqual(
+        square(sec(variable('x')))
+      )
+    })
+
+    it('returns the chain rule of the derivative of the secant', () => {
+      expect(differentiate(sec(variable('x')))).toEqual(
+        multiply(sec(variable('x')), tan(variable('x')))
+      )
+    })
+
+    it('returns the chain rule of the derivative of the cosecant', () => {
+      expect(differentiate(csc(variable('x')))).toEqual(
+        multiply(negate(csc(variable('x'))), cot(variable('x')))
+      )
+    })
+
+    it('returns the chain rule of the derivative of the cotangent', () => {
+      expect(differentiate(cot(variable('x')))).toEqual(
+        negate(square(csc(variable('x'))))
+      )
+    })
+  })
+
+  describe('of arcus functions', () => {
+    it('returns the chain rule of the derivative of the acos', () => {
+      expect(differentiate(acos(variable('x')))).toEqual(
+        negate(reciprocal(sqrt(subtract(real(1), square(variable('x'))))))
+      )
+    })
+
+    it('returns the chain rule of the derivative of the asin', () => {
+      expect(differentiate(asin(variable('x')))).toEqual(
+        reciprocal(sqrt(subtract(real(1), square(variable('x')))))
+      )
+    })
+
+    it('returns the chain rule of the derivative of the atan', () => {
+      expect(differentiate(atan(variable('x')))).toEqual(
+        reciprocal(add(real(1), square(variable('x'))))
+      )
+    })
+
+    it('returns the chain rule of the derivative of the asec', () => {
+      expect(differentiate(asec(variable('x')))).toEqual(
+        reciprocal(multiply(
+          abs(variable('x')),
+          sqrt(subtract(square(variable('x')), real(1)))
+        ))
+      )
+    })
+
+    it('returns the chain rule of the derivative of the acsc', () => {
+      expect(differentiate(acsc(variable('x')))).toEqual(
+        negate(reciprocal(multiply(
+          abs(variable('x')),
+          sqrt(subtract(square(variable('x')), real(1)))
+        )))
+      )
+    })
+
+    it('returns the chain rule of the derivative of the acot', () => {
+      expect(differentiate(acot(variable('x')))).toEqual(
+        negate(reciprocal(add(square(variable('x')), real(1))))
+      )
+    })
+  })
+
+  describe('of hyperbolic functions', () => {
+
+  })
+
+  describe('of area hyperbolic functions', () => {
+
   })
 })
--- client/src/common/Tree/differentiation.ts
+++ client/src/common/Tree/differentiation.ts
@@ -1,46 +1,116 @@
 import { method, multi, Multi } from '@arrows/multimethod'
 import { 
-  Base, Real, Complex, Variable, Addition, Multiplication, Exponentiation
+  Base, Real, Complex, Variable, Addition, Multiplication, Exponentiation,
+  Logarithm, AbsoluteValue,
+  Cosine, Sine, Tangent, Secant, Cosecant, Cotangent,
+  ArcusCosine, ArcusSine, ArcusTangent,
+  ArcusSecant, ArcusCosecant, ArcusCotangent
 } from './Expression'
 import { real } from './real'
 import { complex } from './complex'
-import { add } from './addition'
-import { multiply, divide } from './multiplication'
+import { add, subtract } from './addition'
+import { multiply, divide, negate } from './multiplication'
+import { raise, square, sqrt, reciprocal } from './exponentiation'
 import { ln } from './logarithmic'
+import { abs } from './absolute'
+import { cos, sin, tan, sec, csc, cot } from './trigonometric'
 
 const chain = (derivative: Base, argument: Base) =>
   multiply(derivative, differentiate(argument))
 
-const whenReal = (expression: Real) => real(0)
-const whenComplex = (expression: Complex) => complex(0, 0)
-const whenVariable = (expression: Variable) => real(1)
-const whenAddition = (expression: Addition) =>
-  add(differentiate(expression.left), differentiate(expression.right))
-const whenMultiplication = (expression: Multiplication) =>
+type when<T> = (expression: T) => Base
+
+const whenReal: when<Real> = _ => real(0)
+const whenComplex: when<Complex> = _ => complex(0, 0)
+const whenVariable: when<Variable> = _ => real(1)
+
+const whenAddition: when<Addition> = e =>
+  add(differentiate(e.left), differentiate(e.right))
+
+const whenMultiplication: when<Multiplication> = e =>
   add(
-    multiply(differentiate(expression.left), expression.right),
-    multiply(expression.left, differentiate(expression.right))
+    multiply(differentiate(e.left), e.right),
+    multiply(e.left, differentiate(e.right))
   )
-const whenExponentiation = (expression: Exponentiation) =>
+
+const whenExponentiation: when<Exponentiation> = e =>
   multiply(
-    expression,
+    e,
     add(
-      multiply(
-        differentiate(expression.left),
-        divide(expression.right, expression.left)
-      ),
-      multiply(
-        differentiate(expression.right),
-        ln(expression.left)
-      )
+      multiply(differentiate(e.left), divide(e.right, e.left)),
+      multiply(differentiate(e.right), ln(e.left))
     )
   )
+
+const whenLogarithm: when<Logarithm> = e =>
+  divide(
+    differentiate(e.right), multiply(e.right, ln(e.left))
+  )
+
+const whenAbsolute: when<AbsoluteValue> = e =>
+  chain(divide(e.expression, e), e.expression)
+
+const whenCosine: when<Cosine> = e =>
+  chain(negate(sin(e.expression)), e.expression)
+
+const whenSine: when<Sine> = e =>
+  chain(cos(e.expression), e.expression)
+
+const whenTangent: when<Tangent> = e =>
+  chain(square(sec(e.expression)), e.expression)
+
+const whenSecant: when<Secant> = e =>
+  chain(multiply(sec(e.expression), tan(e.expression)), e.expression)
+
+const whenCosecant: when<Cosecant> = e => 
+  chain(multiply(negate(csc(e.expression)), cot(e.expression)), e.expression)
+
+const whenCotangent: when<Cotangent> = e =>
+  chain(negate(square(csc(e.expression))), e.expression)
+
+const whenArcusCosine: when<ArcusCosine> = e =>
+  negate(divide(
+    differentiate(e.expression),
+    sqrt(subtract(real(1), square(e.expression)))
+  ))
+
+const whenArcusSine: when<ArcusSine> = e =>
+  chain(reciprocal(sqrt(subtract(real(1), square(e.expression)))), e.expression)
+
+const whenArcusTangent: when<ArcusTangent> = e =>
+  chain(reciprocal(add(real(1), square(e.expression))), e.expression)
+
+const whenArcusSecant: when<ArcusSecant> = e =>
+  chain(
+    reciprocal(multiply(
+      abs(e.expression), 
+      sqrt(subtract(square(e.expression), real(1)))
+    )), 
+    e.expression
+  )
+
+const whenArcusCosecant: when<ArcusCosecant> = e =>
+  negate(chain(
+    reciprocal(multiply(
+      abs(e.expression), 
+      sqrt(subtract(square(e.expression), real(1)))
+    )), 
+    e.expression
+  ))
+
+const whenArcusCotangent: when<ArcusCotangent> = e =>
+  negate(chain(reciprocal(add(square(e.expression), real(1))), e.expression))
+  
 const whenBase = (expression: Base) => expression
 
 export type DifferentiateFn = Multi
   & typeof whenReal & typeof whenComplex & typeof whenVariable
   & typeof whenAddition & typeof whenMultiplication 
-  & typeof whenExponentiation
+  & typeof whenExponentiation & typeof whenLogarithm & typeof whenAbsolute
+  & typeof whenCosine & typeof whenSine & typeof whenTangent
+  & typeof whenSecant & typeof whenCosecant & typeof whenCotangent
+  & typeof whenArcusCosine & typeof whenArcusSine & typeof whenArcusTangent
+  & typeof whenArcusSecant & typeof whenArcusCosecant & typeof whenArcusCotangent
   & typeof whenBase
 
 export const differentiate: DifferentiateFn = multi(
@@ -49,6 +119,20 @@ export const differentiate: DifferentiateFn = multi(
   method(Variable, whenVariable),
   method(Addition, whenAddition),
   method(Multiplication, whenMultiplication),
-  method(Exponentiation, whenExponentiation)
+  method(Exponentiation, whenExponentiation),
+  method(Logarithm, whenLogarithm),
+  method(AbsoluteValue, whenAbsolute),
+  method(Cosine, whenCosine),
+  method(Sine, whenSine),
+  method(Tangent, whenTangent),
+  method(Secant, whenSecant),
+  method(Cosecant, whenCosecant),
+  method(Cotangent, whenCotangent),
+  method(ArcusCosine, whenArcusCosine),
+  method(ArcusSine, whenArcusSine),
+  method(ArcusTangent, whenArcusTangent),
+  method(ArcusSecant, whenArcusSecant),
+  method(ArcusCosecant, whenArcusCosecant),
+  method(ArcusCotangent, whenArcusCotangent)
   // method(Base, whenBase)
 )
