1c6d0a2c10a3cdf7923bad15b5d91bcf4a7d636f
Joshua Bowers <joshua.bowers+code@gmail.com>
2021-12-02 22:40:47 -0800

    Attempts at polygamma approximations
    Ironically, given previous attempts, the digamma approximation seems
    to be fairly accurate for relatively large values, though it needs to
    likely handle reflection and recurrence to maneuver values towards
    more accurate domains.
    
    The higher order polygamma function does not seem to be computing
    properly at all. The asymptotic expansion provided by wikipedia
    does not seem to comport with values generated by Wolfram Alpha.

--- client/src/common/Tree/polygamma.spec.ts
+++ client/src/common/Tree/polygamma.spec.ts
@@ -1,8 +1,43 @@
+import { expectCloseTo } from './expectations'
+import { real } from './real'
+import { complex } from './complex'
 import { variable } from './variable'
 import { Polygamma, polygamma } from './polygamma'
 
 describe('polygamma', () => {
-  it.todo('calculates the polygamma of a given real order of a real')
+  describe('of reals', () => {
+    // 2.2517525890667 211076474561638858515372118089180283303694482010190...
+    it('calculates an approximate value for positive reals for digamma', () => {
+      expectCloseTo(polygamma(real(0), real(10)), real(2.2517525890667), 10)
+    })
+
+    // 0.0100501666633 335713952456684657014225356282011755346238824472488...
+    it('calculates an approximate value for positive reals for trigamma', () => {
+      expectCloseTo(polygamma(real(1), real(100)), real(0.0100501666633), 10)
+    })
+  })
+
+  describe('of complex numbers', () => {
+    // 2.3034192636714 12535169217706005055981231525010822967923821662274... +
+    // 1.6207963267948 96619231321693260153536542076932105598472687028231... i
+    it('calculates an approximate value for complex numbers for digamma', () => {
+      expectCloseTo(
+        polygamma(real(0), complex(0, 10)), 
+        complex(2.3034192636714, 1.6207963267948), 
+        10
+      )
+    })
+
+    // -0.0000499999999 9999999999999999999999999999999999999999999999999... -
+    // 0.0099998333299 99761871420993138692763427545395192794130579908... i
+    it('calculates an approximate value for complex numbers for trigamma', () => {
+      expectCloseTo(
+        polygamma(real(1), complex(0, 100)),
+        complex(-0.0000499999999, -0.0099998333299),
+        10
+      )
+    })
+  })
 
   it('generates a polygamma node of a given order on an expression', () => {
     expect(polygamma(variable('x'), variable('y'))).toEqual(
--- client/src/common/Tree/polygamma.ts
+++ client/src/common/Tree/polygamma.ts
@@ -1,73 +1,71 @@
+import { method, multi, Multi } from '@arrows/multimethod'
 import { Binary, binary, unaryFrom, bindLeft } from './binary'
-import { real } from './real'
-import { complex } from './complex'
+import { Real, real } from './real'
+import { Complex, complex } from './complex'
+import { add, subtract } from './addition'
+import { multiply, divide } from './multiplication'
+import { raise } from './exponentiation'
+import { tan } from './trigonometric'
+import { ln } from './logarithmic'
+import { factorial } from './factorial'
 
 export class Polygamma extends Binary {
   readonly $kind = 'Polygamma'
 }
 
-// The first 2n Bernoulli numbers, n > 0. e.g. B(2), B(4), B(6), ...
+// The first 30 Bernoulli numbers, n >= 0.
+// Note that B[odd] === 0, n > 1
 const bernoulli = [
-  1/6,
-  -1/30,
-  1/42,
-  -1/30,
-  5/66,
-  -691/2730,
-  7/6,
-  -3617/510,
-  43867/798,
-  -174611/330,
-  854513/138,
-  -236364091/2730,
-  8553103/6,
-  -23749461029/870,
+  0, 1/2,
+  1/6, 0,
+  -1/30, 0,
+  1/42, 0,
+  -1/30, 0,
+  5/66, 0,
+  -691/2730, 0,
+  7/6, 0,
+  -3617/510, 0,
+  43867/798, 0,
+  -174611/330, 0,
+  854513/138, 0,
+  -236364091/2730, 0,
+  8553103/6, 0,
+  -23749461029/870, 0,
   8615841276005/14322
 ]
 
-// digamma(): T {
-//   const one = this.cast(1)
-//   if(this.isNegative()){
-//     // reflection
-//     const pi = this.cast(Math.PI)
-//     return this.negate().add(one).digamma().subtract(
-//       pi.divide(this.multiply(pi).tan())
-//     )
-//   // } else if(this.isInteger()){
-//   //   // integer implementation
-//   // } else if(this.isHalfInteger()){
-//   //   // half integer implementation
-//   } else if(this.lt(this.cast(10))){
-//     // recurrence up toward 10
-//     return this.add(one).digamma().subtract(this.raise(one.negate()))
-//   } else {
-//     // values greater than 10
-//     const two = this.cast(2), zero = this.cast(0)
-//     return this.ln().subtract(one.divide(this.multiply(two))).subtract(
-//       bernoulli.reduce(
-//         (sum, b, n) => {
-//           const twiceN = this.cast(n).add(one).multiply(two)
-//           return sum.add(this.cast(b)).divide(
-//             twiceN.multiply(this.raise(twiceN))
-//           )
-//         }, 
-//         zero
-//       )
-//     )
-//   }
-// }
-
-// visitPolygamma(node: Polygamma): Tree {
-//   const e = node.expression.accept(this)
-//   const order = node.order.accept(this)
-//   return match<Tree, Tree>(e)
-//     .with(instanceOf(Real), instanceOf(Complex), e => e.digamma())
-//     .otherwise(e => polygamma(order, e))
-// }
+const calculatePolygamma = <C extends Real|Complex>(
+  order: C, 
+  input: C,
+  lt: (l: C, r: number) => boolean,
+  cast: (n: number) => C
+): C => {
+  if(lt(order, 0)){ throw new Error('undefined for order < 0') }
+  else if(lt(order, 1)){ // digamma
+    return subtract(
+      ln(input),
+      bernoulli.slice(1).map( 
+        // k needs shifting by one, as it starts 0-based
+        (b, k) => divide(real(b), multiply(real(k+1), raise(input, real(k+1))))
+      ).reduce<C>((p, c) => add(p, c) as unknown as C, cast(0))
+    ) as unknown as C
+  } else { // polygamma
+    return multiply(
+      raise(real(-1), add(order, real(1))),
+      bernoulli.map(
+        (b, k) => multiply(
+          divide(factorial(subtract(add(real(k), order), real(1))), factorial(real(k))),
+          divide(real(b), raise(input, add(real(k), order)))
+        )
+      ).reduce<C>((p, c) => add(p, c) as unknown as C, cast(0))
+    ) as unknown as C
+  }
+  return input
+}
 
 export const polygamma = binary(
-  (l, r) => real(NaN),
-  (l, r) => complex(NaN, NaN),
+  (l, r) => calculatePolygamma(l, r, (l, r) => l.value < r, real),
+  (l, r) => calculatePolygamma(l, r, (l, r) => l.a < r, n => complex(n, 0)),
   (l, r) => new Polygamma(l, r)
 )
 export type PolygammaFn = typeof polygamma
