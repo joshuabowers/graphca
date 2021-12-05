9015f2625a03b16916d50f09bbd0b34cc6bd1079
Joshua Bowers <joshua.bowers+code@gmail.com>
2021-12-04 16:38:57 -0800

    Trigoid reciprocals invocable from UI
    Long-time coming; finally added in. Huzzah.

--- client/src/layouts/Arithmetic/index.tsx
+++ client/src/layouts/Arithmetic/index.tsx
@@ -13,19 +13,19 @@ export const Arithmetic = (props: ArithmeticProps) => (
       default={{type: 'default', display: Unicode.division, displayHint: 'icon', activate: createKeyPress(Unicode.division)}}
       shift={{type: 'shift', display: functional.invert, displayHint: 'functional', activate: createKeyPress('^-1')}}
       alpha={{type: 'alpha', display: 'S', activate: createKeyPress('S')}}
-      trig={{type:'trig', display:''}}
+      trig={{type:'trig', display:'acsch', displayHint: 'verbose', activate: createKeyPress('acsch(')}}
     />
     <Key 
       default={{type: 'default', display: Unicode.multiplication, displayHint: 'icon', activate: createKeyPress(Unicode.multiplication)}}
       shift={{type: 'shift', display: 'EE', activate: createKeyPress('E')}}
       alpha={{type: 'alpha', display: 'W', activate: createKeyPress('W')}}
-      trig={{type:'trig', display:''}}
+      trig={{type:'trig', display:'asech', displayHint: 'verbose', activate: createKeyPress('asech(')}}
     />
     <Key 
       default={{type: 'default', display: Unicode.minus, displayHint: 'icon', activate: createKeyPress(Unicode.minus)}}
       shift={{type: 'shift', display: ''}}
       alpha={{type: 'alpha', display: ''}}
-      trig={{type:'trig', display:''}}
+      trig={{type:'trig', display:'acoth', displayHint: 'verbose', activate: createKeyPress('acoth(')}}
     />
     <Key 
       default={{type: 'default', display: '+', displayHint: 'icon', activate: createKeyPress('+')}}
--- client/src/layouts/Numeric/index.tsx
+++ client/src/layouts/Numeric/index.tsx
@@ -9,15 +9,15 @@ export interface NumericProps {
 
 // Tuple structure: ['default', 'shift', 'alpha', 'trig']
 const numberKeys = [
-  ['7', '', 'P', ''],
-  ['8', '', 'Q', ''],
-  ['9', '', 'R', ''],
-  ['4', '', 'T', ''],
-  ['5', '', 'U', ''],
-  ['6', '', 'V', ''],
-  ['1', Unicode.i, 'X', ''],
-  ['2', Unicode.e, 'Y', ''],
-  ['3', Unicode.pi, 'Z', ''],
+  ['7', '', 'P', 'csc'],
+  ['8', '', 'Q', 'csch'],
+  ['9', '', 'R', 'acsc'],
+  ['4', '', 'T', 'sec'],
+  ['5', '', 'U', 'sech'],
+  ['6', '', 'V', 'asec'],
+  ['1', Unicode.i, 'X', 'cot'],
+  ['2', Unicode.e, 'Y', 'coth'],
+  ['3', Unicode.pi, 'Z', 'acot'],
   ['0', Unicode.infinity, Unicode.space, ''],
   ['.', Unicode.epsilon, ':', ''],
   ['<-', '', '', '']
@@ -35,7 +35,7 @@ export const Numeric = (props: NumericProps) => {
             default={{type: 'default', display: info[0], activate: createKeyPress(info[0])}}
             shift={{type: 'shift', display: info[1], activate: info[1] ? createKeyPress(info[1]) : undefined}}
             alpha={{type: 'alpha', display: info[2], activate: createKeyPress(unicodeToASCII.get(info[2]) ?? info[2])}}
-            trig={{type: 'trig', display: info[3], activate: info[3] ? createKeyPress(info[3]) : undefined}}
+            trig={{type: 'trig', display: info[3], activate: info[3] ? createKeyPress(info[3]+'(') : undefined}}
           />  
         ))
       }
