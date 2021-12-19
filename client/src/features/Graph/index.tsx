278c69e18204b0d14f43afeca53cc54c794f24a2
Joshua Bowers <joshua.bowers+code@gmail.com>
2021-12-18 18:32:12 -0800

    Responsive curves
    - Graphs now have MapControls for panning and zooming. Will need to
      change to OrbitControls for 3d.
    - Expressions are only evaluated between the boundary defined by the
      left and right side of the current viewport.
    - Slices cut to a fourth.
    - As the viewport pans or zooms, the points for the curve are
      recalculated using the new boundaries.

--- client/src/features/Graph/index.tsx
+++ client/src/features/Graph/index.tsx
@@ -1,16 +1,14 @@
 import * as THREE from 'three'
-import React, { useState, useRef, useMemo } from 'react'
-import styles from './Graph.module.css';
-import { Canvas, useFrame } from '@react-three/fiber';
+import React, { useRef, useMemo, useState } from 'react'
+// import styles from './Graph.module.css';
+import { Canvas, useThree, useFrame } from '@react-three/fiber';
 import { Base, Real, real, invoke } from '../../common/Tree'
 import { RootState } from '../../app/store';
-import { describe } from '../../common/description'
-import { createSelector } from 'reselect';
 import { createArraySelector } from 'reselect-map';
 import { parser } from '../../common/parser';
 import { useAppSelector } from '../../app/hooks';
 import { Color, Vector3 } from 'three';
-import { Line } from '@react-three/drei';
+import { Line, MapControls } from '@react-three/drei';
 
 export interface GraphProps {
 
@@ -19,17 +17,16 @@ export interface GraphProps {
 function valuesBetween(expression: Base, min: number, max: number, slices: number = 100) {
   const increment = (max - min) / slices
   const r = new Array<Vector3>(slices);
+  const valueAt = invoke()(expression)
   let j = 0;
   for(let i = min; i < max; i += increment) {
-    const y = invoke()(expression)(real(i))
+    const y = valueAt(real(i))
     if(!(y instanceof Real)){ throw new Error(`Unexpected invocation result: ${y.$kind}`)}
     r[j++] = new Vector3(i, y.value, 0)
   }
   return r
 }
 
-const getGraph = (state: RootState) => state.graph
-const getExpressions = createSelector(getGraph, (graph) => graph.expressions)
 const getParsings = createArraySelector<RootState, string, Base>(
   (state) => state.graph.expressions,
   (input) => parser.value(input)
@@ -40,34 +37,48 @@ interface CurveProps {
   color: Color
 }
 
+type Range = {from: number, to: number, width: number, center: number}
+
+const boundary = (camera: THREE.Camera, width: number): Range => ({
+  center: camera.position.x,
+  from: camera.position.x - width/2,
+  to: camera.position.x + width/2,
+  width
+})
+
 const Curve = (props: CurveProps) => {
-  console.info('generating curve for', props.expression)
+  const { camera, viewport } = useThree();
+  const [range, setRange] = useState<Range>(boundary(camera, viewport.width))
+
+  useFrame(({camera, viewport}, d) => {
+    const vp = viewport.getCurrentViewport(camera)
+    if(range.width !== vp.width || range.center !== camera.position.x){ 
+      setRange(boundary(camera, vp.width)) 
+    }
+  })
+
   const points = useMemo(
-    () => valuesBetween(props.expression, -100, 100, 1000),
-    [props.expression]
+    () => valuesBetween(
+      props.expression, 
+      range.from, 
+      range.to,
+      250
+    ),
+    [props.expression, range]
   )
-  console.info('points:', points)
-  return <Line 
+  return range.width > 0 && points.length > 0 ? <Line 
     points={points}
     color={props.color}
-  />
+    lineWidth={1}
+  /> : <></>
 }
 
 export const Graph = (props: GraphProps) => {
   const canvasRef = useRef<HTMLCanvasElement>(null)
   const parsings = useAppSelector(getParsings)
 
-  const color = useMemo(
-    () => new THREE.Color(
-      canvasRef.current 
-        ? window.getComputedStyle(canvasRef.current).getPropertyValue('--curve-color') 
-        : 'green'
-    ),
-    [styles, canvasRef]
-  )
-
   return (
-    <Canvas ref={canvasRef}>
+    <Canvas orthographic camera={{position: [0, 0, 1], zoom: 30, up: [0, 0, 1]}} ref={canvasRef}>
       <ambientLight />
       <pointLight position={[10, 10, 10]} />
       <gridHelper 
@@ -77,10 +88,15 @@ export const Graph = (props: GraphProps) => {
       <group>
       {
         parsings.map((v,i) => 
-          <Curve expression={v} color={color} key={i} />
+          <Curve 
+            expression={v} 
+            color={new THREE.Color('green')} 
+            key={i} 
+          />
         )
       }
       </group>
+      <MapControls />
     </Canvas>
   )
 }
