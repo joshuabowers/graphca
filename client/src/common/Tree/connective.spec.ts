e2b09078e2b689bc6253034c4a1df523badfdeb2
Joshua Bowers <joshua.bowers+code@gmail.com>
2022-07-25 16:55:56 -0700

    Added more logical connectives
    'and', 'or' are joined by 'xor', 'implies', 'nand', 'nor', 'xnor',
    and 'converse'. These round out the connectives. Note that, while
    implemented differently, 'xnor' is equivalent to 'equals'.

--- client/src/common/Tree/connective.spec.ts
+++ client/src/common/Tree/connective.spec.ts
@@ -3,8 +3,10 @@ import { complex } from './complex'
 import { bool } from './boolean'
 import { variable } from './variable'
 import {
-  Conjunction, Disjunction,
-  and, or
+  Conjunction, Disjunction, ExclusiveDisjunction, Implication,
+  AlternativeDenial, JointDenial, Biconditional, ConverseImplication,
+  and, or, xor, implies,
+  nand, nor, xnor, converse
 } from './connective'
 
 describe('and', () => {
@@ -67,6 +69,150 @@ describe('or', () => {
   it('returns a Disjunction on variable input', () => {
     expect(or(variable('x'), variable('y'))).toEqual(
       new Disjunction(variable('x'), variable('y'))
+    )
+  })
+})
+
+describe('xor', () => {
+  it('returns false when given two true things', () => {
+    expect(xor(bool(true), bool(true))).toEqual(bool(false))
+  })
+
+  it('returns true if the left argument is false', () => {
+    expect(xor(bool(false), bool(true))).toEqual(bool(true))
+  })
+
+  it('returns true if the right argument is false', () => {
+    expect(xor(bool(true), bool(false))).toEqual(bool(true))
+  })
+
+  it('returns false if both arguments are false', () => {
+    expect(xor(bool(false), bool(false))).toEqual(bool(false))
+  })
+
+  it('returns a ExclusiveDisjunction on variable input', () => {
+    expect(xor(variable('x'), variable('y'))).toEqual(
+      new ExclusiveDisjunction(variable('x'), variable('y'))
+    )
+  })  
+})
+
+describe('implies', () => {
+  it('returns true when given two true things', () => {
+    expect(implies(bool(true), bool(true))).toEqual(bool(true))
+  })
+
+  it('returns true if the left argument is false', () => {
+    expect(implies(bool(false), bool(true))).toEqual(bool(true))
+  })
+
+  it('returns false if the right argument is false', () => {
+    expect(implies(bool(true), bool(false))).toEqual(bool(false))
+  })
+
+  it('returns true if both arguments are false', () => {
+    expect(implies(bool(false), bool(false))).toEqual(bool(true))
+  })
+  
+  it('returns an Implication on variable input', () => {
+    expect(implies(variable('x'), variable('y'))).toEqual(
+      new Implication(variable('x'), variable('y'))
+    )
+  })
+})
+
+describe('nand', () => {
+  it('returns false when given two true things', () => {
+    expect(nand(bool(true), bool(true))).toEqual(bool(false))
+  })
+
+  it('returns true if the left argument is false', () => {
+    expect(nand(bool(false), bool(true))).toEqual(bool(true))
+  })
+
+  it('returns true if the right argument is false', () => {
+    expect(nand(bool(true), bool(false))).toEqual(bool(true))
+  })
+
+  it('returns true if both arguments are false', () => {
+    expect(nand(bool(false), bool(false))).toEqual(bool(true))
+  })
+
+  it('returns a AlternativeDenial on variable input', () => {
+    expect(nand(variable('x'), variable('y'))).toEqual(
+      new AlternativeDenial(variable('x'), variable('y'))
+    )
+  })
+})
+
+describe('nor', () => {
+  it('returns false when given two true things', () => {
+    expect(nor(bool(true), bool(true))).toEqual(bool(false))
+  })
+
+  it('returns false if the left argument is false', () => {
+    expect(nor(bool(false), bool(true))).toEqual(bool(false))
+  })
+
+  it('returns false if the right argument is false', () => {
+    expect(nor(bool(true), bool(false))).toEqual(bool(false))
+  })
+
+  it('returns true if both arguments are false', () => {
+    expect(nor(bool(false), bool(false))).toEqual(bool(true))
+  })
+
+  it('returns a JointDenial on variable input', () => {
+    expect(nor(variable('x'), variable('y'))).toEqual(
+      new JointDenial(variable('x'), variable('y'))
+    )
+  })
+})
+
+describe('xnor', () => {
+  it('returns true when given two true things', () => {
+    expect(xnor(bool(true), bool(true))).toEqual(bool(true))
+  })
+
+  it('returns false if the left argument is false', () => {
+    expect(xnor(bool(false), bool(true))).toEqual(bool(false))
+  })
+
+  it('returns false if the right argument is false', () => {
+    expect(xnor(bool(true), bool(false))).toEqual(bool(false))
+  })
+
+  it('returns true if both arguments are false', () => {
+    expect(xnor(bool(false), bool(false))).toEqual(bool(true))
+  })
+
+  it('returns a Biconditional on variable input', () => {
+    expect(xnor(variable('x'), variable('y'))).toEqual(
+      new Biconditional(variable('x'), variable('y'))
+    )
+  })
+})
+
+describe('converse', () => {
+  it('returns true when given two true things', () => {
+    expect(converse(bool(true), bool(true))).toEqual(bool(true))
+  })
+
+  it('returns false if the left argument is false', () => {
+    expect(converse(bool(false), bool(true))).toEqual(bool(false))
+  })
+
+  it('returns true if the right argument is false', () => {
+    expect(converse(bool(true), bool(false))).toEqual(bool(true))
+  })
+
+  it('returns true if both arguments are false', () => {
+    expect(converse(bool(false), bool(false))).toEqual(bool(true))
+  })
+
+  it('returns a ConverseImplication on variable input', () => {
+    expect(converse(variable('x'), variable('y'))).toEqual(
+      new ConverseImplication(variable('x'), variable('y'))
     )
   })
 })
--- client/src/common/Tree/connective.ts
+++ client/src/common/Tree/connective.ts
@@ -1,5 +1,7 @@
+import { Base } from './Expression'
 import { Boolean, bool } from './boolean'
 import { Binary, binary } from './binary'
+import { not } from './logicalComplement'
 
 export abstract class Connective extends Binary {}
 
@@ -9,6 +11,30 @@ export class Conjunction extends Connective {
 
 export class Disjunction extends Connective {
   readonly $kind = 'Disjunction'
+}
+
+export class ExclusiveDisjunction extends Connective {
+  readonly $kind = 'ExclusiveDisjunction'
+}
+
+export class Implication extends Connective {
+  readonly $kind = 'Implication'
+}
+
+export class AlternativeDenial extends Connective {
+  readonly $kind = 'AlternativeDenial'
+}
+
+export class JointDenial extends Connective {
+  readonly $kind = 'JointDenial'
+}
+
+export class Biconditional extends Connective {
+  readonly $kind = 'Biconditional'
+}
+
+export class ConverseImplication extends Connective {
+  readonly $kind = 'ConverseImplication'
 }
 
 export const and = binary(Conjunction, Boolean)(
@@ -21,4 +47,43 @@ export const or = binary(Disjunction, Boolean)(
   (l, r) => bool(l.value !== 0 || r.value !== 0),
   (l, r) => bool(l.a !== 0 || l.b !== 0 || r.a !== 0 || r.b !== 0),
   (l, r) => bool(l.value || r.value)
+)
+
+export const xor = binary(ExclusiveDisjunction, Boolean)(
+  (l, r) => and(or(l, r), not(and(l, r))),
+  (l, r) => and(or(l, r), not(and(l, r))),
+  (l, r) => and(or(l, r), not(and(l, r)))
+)
+
+export const implies = binary(Implication, Boolean)(
+  (l, r) => or(not(l), r),
+  (l, r) => or(not(l), r),
+  (l, r) => or(not(l), r)
+)
+
+// NOTE: nand/nor implemented directly rather than as connective 
+// relationships due to 'not' having Boolean|Real return types.
+
+export const nand = binary(AlternativeDenial, Boolean)(
+  (l, r) => bool(!(l.value !== 0 && r.value !== 0)),
+  (l, r) => bool(!((l.a !== 0 || l.b !== 0) && (r.a !== 0 || r.b !== 0))),
+  (l, r) => bool(!(l.value && r.value))
+)
+
+export const nor = binary(JointDenial, Boolean)(
+  (l, r) => bool(!(l.value !== 0 || r.value !== 0)),
+  (l, r) => bool(!(l.a !== 0 || l.b !== 0 || r.a !== 0 || r.b !== 0)),
+  (l, r) => bool(!(l.value || r.value))
+)
+
+export const xnor = binary(Biconditional, Boolean)(
+  (l, r) => and(implies(l, r), implies(r, l)),
+  (l, r) => and(implies(l, r), implies(r, l)),
+  (l, r) => and(implies(l, r), implies(r, l))
+)
+
+export const converse = binary(ConverseImplication, Boolean)(
+  (l, r) => or(l, not(r)),
+  (l, r) => or(l, not(r)),
+  (l, r) => or(l, not(r))
 )
