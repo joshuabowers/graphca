41294f72922ded1ee423a346e22e764e45e6b46e
Joshua Bowers <joshua.bowers+code@gmail.com>
2022-08-20 00:34:07 -0700

    apps/frontend seems to use packages/calcula
    Not entirely definitive, but the former seems to be properly importing
    the latter with type information. Progress.

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Unicode } from '@bowers/calcula'
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
         >
           Learn React
         </a>
+        <p>{Unicode.nand}</p>
       </header>
     </div>
   );
