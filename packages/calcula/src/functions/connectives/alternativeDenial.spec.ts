c82b5944970a692ea4ddde38c182086f8d5a4531
Joshua Bowers <joshua.bowers+code@gmail.com>
2022-08-30 16:33:56 -0700

    Alternative denial code and spec updated
    Using the Writer/Action infrastructure.

--- packages/calcula/src/functions/connectives/alternativeDenial.spec.ts
+++ packages/calcula/src/functions/connectives/alternativeDenial.spec.ts
@@ -9,62 +9,141 @@ import { AlternativeDenial, nand } from './alternativeDenial'
 
 describe('nand', () => {
   it('returns false when given two true things', () => {
-    expect(nand(boolean(true), boolean(true))).toEqual(boolean(false))
+    expectWriter(
+      nand(boolean(true), boolean(true))
+    )(
+      boolean(false),
+      [[boolean(true), boolean(true)], 'alternative deniable complementation'],
+      [boolean(true), 'boolean complement']
+    )
   })
 
   it('returns true if the left argument is false', () => {
-    expect(nand(boolean(false), boolean(true))).toEqual(boolean(true))
+    expectWriter(
+      nand(boolean(false), boolean(true))
+    )(
+      boolean(true),
+      [[boolean(false), boolean(true)], 'alternative deniable complementation'],
+      [boolean(false), 'boolean complement']
+    )
   })
 
   it('returns true if the right argument is false', () => {
-    expect(nand(boolean(true), boolean(false))).toEqual(boolean(true))
+    expectWriter(
+      nand(boolean(true), boolean(false))
+    )(
+      boolean(true),
+      [[boolean(true), boolean(false)], 'alternative deniable complementation'],
+      [boolean(false), 'boolean complement']
+    )
   })
 
   it('returns true if both arguments are false', () => {
-    expect(nand(boolean(false), boolean(false))).toEqual(boolean(true))
+    expectWriter(
+      nand(boolean(false), boolean(false))
+    )(
+      boolean(true),
+      [[boolean(false), boolean(false)], 'alternative deniable annihilator']
+    )
   })
 
   it('returns the complement of left operand if the right is true', () => {
-    expect(nand(variable('x'), boolean(true))).toEqual(not(variable('x')))
+    expectWriter(
+      nand(variable('x'), boolean(true))
+    )(
+      not(variable('x')),
+      [[variable('x'), boolean(true)], 'alternative deniable complementation'],
+      [variable('x'), 'complement']
+    )
   })
 
   it('returns the right operand if the left is true', () => {
-    expect(nand(boolean(true), variable('x'))).toEqual(not(variable('x')))
+    expectWriter(
+      nand(boolean(true), variable('x'))
+    )(
+      not(variable('x')),
+      [[boolean(true), variable('x')], 'alternative deniable complementation'],
+      [variable('x'), 'complement']
+    )
   })
 
   it('returns the true if the right operand is false', () => {
-    expect(nand(variable('x'), boolean(false))).toEqual(boolean(true))
+    expectWriter(
+      nand(variable('x'), boolean(false))
+    )(
+      boolean(true),
+      [[variable('x'), boolean(false)], 'alternative deniable annihilator']
+    )
   })
 
   it('returns true if the left operand is false', () => {
-    expect(nand(boolean(false), variable('x'))).toEqual(boolean(true))
+    expectWriter(
+      nand(boolean(false), variable('x'))
+    )(
+      boolean(true),
+      [[boolean(false), variable('x')], 'alternative deniable annihilator']
+    )
   })
 
   it('returns the complement of the left operand if left equivalent to right', () => {
-    expect(nand(variable('x'), variable('x'))).toEqual(not(variable('x')))
+    expectWriter(
+      nand(variable('x'), variable('x'))
+    )(
+      not(variable('x')),
+      [[variable('x'), variable('x')], 'alternative deniable complementation'],
+      [variable('x'), 'complement']
+    )
   })
 
   it('returns an implication if the right operand is a nand of the left', () => {
-    expect(
+    expectWriter(
       nand(variable('x'), nand(variable('x'), variable('y')))
-    ).toEqual(implies(variable('x'), variable('y')))
-    expect(
+    )(
+      implies(variable('x'), variable('y')),
+      [[variable('x'), variable('y')], 'alternative denial'],
+      [[variable('x'), nand(variable('x'), variable('y'))], 'alternative deniable implication'],
+      [[variable('x'), variable('y')], 'implication']
+    )
+    expectWriter(
       nand(variable('x'), nand(variable('y'), variable('x')))
-    ).toEqual(implies(variable('x'), variable('y')))
+    )(
+      implies(variable('x'), variable('y')),
+      [[variable('y'), variable('x')], 'alternative denial'],
+      [[variable('x'), nand(variable('y'), variable('x'))], 'alternative deniable implication'],
+      [[variable('x'), variable('y')], 'implication']
+    )
   })
 
   it('returns a disjunction of mutually complemented operands', () => {
-    expect(
+    expectWriter(
       nand(not(variable('x')), not(variable('y')))
-    ).toEqual(or(variable('x'), variable('y')))
+    )(
+      or(variable('x'), variable('y')),
+      [variable('x'), 'complement'],
+      [variable('y'), 'complement'],
+      [[not(variable('x')), not(variable('y'))], 'De Morgan'],
+      [[variable('x'), variable('y')], 'disjunction']
+    )
   })
 
   it('returns true if the right operand is the complement of the left', () => {
-    expect(nand(variable('x'), not(variable('x')))).toEqual(boolean(true))
+    expectWriter(
+      nand(variable('x'), not(variable('x')))
+    )(
+      boolean(true),
+      [variable('x'), 'complement'],
+      [[variable('x'), not(variable('x'))], 'alternative deniable annihilator']
+    )
   })
 
   it('returns true if the left operand is the complement of the right', () => {
-    expect(nand(not(variable('x')), variable('x'))).toEqual(boolean(true))
+    expectWriter(
+      nand(not(variable('x')), variable('x'))
+    )(
+      boolean(true),
+      [variable('x'), 'complement'],
+      [[not(variable('x')), variable('x')], 'alternative deniable annihilator']
+    )
   })
 
   it('returns a AlternativeDenial on variable input', () => {
--- packages/calcula/src/functions/connectives/alternativeDenial.ts
+++ packages/calcula/src/functions/connectives/alternativeDenial.ts
@@ -1,12 +1,14 @@
 import { _ } from '@arrows/multimethod'
 import { Writer, unit } from "../../monads/writer"
-import { Clades, Genera, Species, isSpecies, isGenus } from "../../utility/tree"
+import { TreeNode, Clades, Genera, Species, isSpecies, isGenus } from "../../utility/tree"
 import { Real, Complex, Boolean, boolean } from "../../primitives"
 import { BinaryNode, binary, when } from "../../closures/binary"
 import { deepEquals, isValue } from "../../utility/deepEquals"
 import { Connective } from './connective'
-import { not } from './complement'
+import { Complement, isComplement, not } from './complement'
 import { and } from './conjunction'
+import { or } from './disjunction'
+import { implies } from './implication'
 
 export type AlternativeDenial = Connective<Species.nand>
 
@@ -17,17 +19,43 @@ export const [nand, isAlternativeDenial, $nand] = binary<AlternativeDenial, Bool
   (l, r) => [not(and(unit(l), unit(r))), 'complex alternative denial'],
   (l, r) => [not(and(unit(l), unit(r))), 'boolean alternative denial']
 )(
-  // method([_, bool(true)], (l: Base, _r: Boolean) => not(l)),
-  // method([bool(true), _], (_l: Boolean, r: Base) => not(r)),
-  // method([_, bool(false)], bool(true)),
-  // method([bool(false), _], bool(true)),
-  // method(equals, (l: Base, _r: Base) => not(l)),
-  // visit(Base, AlternativeDenial)(identity, leftChild)((l, r) => implies(l, r.right)),
-  // visit(Base, AlternativeDenial)(identity, rightChild)((l, r) => implies(l, r.left)),
-  // method(
-  //   [is(LogicalComplement), is(LogicalComplement)], 
-  //   (l: LogicalComplement, r: LogicalComplement) => or(l.expression, r.expression)
-  // ),
-  // visit(Base, LogicalComplement)(identity, child)((_l, _r) => bool(true)),
-  // visit(LogicalComplement, Base)(child, identity)((_l, _r) => bool(true)) 
+  when(
+    [_, isValue(boolean(true))],
+    (l, _r) => [not(unit(l)), 'alternative deniable complementation']
+  ),
+  when(
+    [isValue(boolean(true)), _],
+    (_l, r) => [not(unit(r)), 'alternative deniable complementation']
+  ),
+  when(
+    [_, isValue(boolean(false))],
+    [boolean(true), 'alternative deniable annihilator']
+  ),
+  when(
+    [isValue(boolean(false)), _],
+    [boolean(true), 'alternative deniable annihilator']
+  ),
+  when(
+    deepEquals, (l, _r) => [not(unit(l)), 'alternative deniable complementation']
+  ),
+  when<TreeNode, AlternativeDenial>(
+    (l, r) => isAlternativeDenial(r) && deepEquals(l, r.value.left),
+    (l, r) => [implies(unit(l), r.right), 'alternative deniable implication']
+  ),
+  when<TreeNode, AlternativeDenial>(
+    (l, r) => isAlternativeDenial(r) && deepEquals(l, r.value.right),
+    (l, r) => [implies(unit(l), r.left), 'alternative deniable implication']
+  ),
+  when<Complement, Complement>(
+    (l, r) => isComplement(l) && isComplement(r),
+    (l, r) => [or(l.expression, r.expression), 'De Morgan']
+  ),
+  when(
+    (l, r) => isComplement(r) && deepEquals(l, r.value.expression),
+    [boolean(true), 'alternative deniable annihilator']
+  ),
+  when(
+    (l, r) => isComplement(l) && deepEquals(l.value.expression, r),
+    [boolean(true), 'alternative deniable annihilator']
+  )
 )
