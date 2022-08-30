6ebbea5418de28a99316c38955951c131698a6e8
Joshua Bowers <joshua.bowers+code@gmail.com>
2022-08-29 18:10:36 -0700

    Joint Denial code and spec updated
    Using Writer and Action to generate output.

--- packages/calcula/src/functions/connectives/jointDenial.spec.ts
+++ packages/calcula/src/functions/connectives/jointDenial.spec.ts
@@ -8,53 +8,120 @@ import { JointDenial, nor } from './jointDenial'
 
 describe('nor', () => {
   it('returns false when given two true things', () => {
-    expect(nor(boolean(true), boolean(true))).toEqual(boolean(false))
+    expectWriter(
+      nor(boolean(true), boolean(true))
+    )(
+      boolean(false),
+      [[boolean(true), boolean(true)], 'joint denial annihilator']
+    )
   })
 
   it('returns false if the left argument is false', () => {
-    expect(nor(boolean(false), boolean(true))).toEqual(boolean(false))
+    expectWriter(
+      nor(boolean(false), boolean(true))
+    )(
+      boolean(false),
+      [[boolean(false), boolean(true)], 'joint denial annihilator']
+    )
   })
 
   it('returns false if the right argument is false', () => {
-    expect(nor(boolean(true), boolean(false))).toEqual(boolean(false))
+    expectWriter(
+      nor(boolean(true), boolean(false))
+    )(
+      boolean(false),
+      [[boolean(true), boolean(false)], 'joint denial annihilator']
+    )
   })
 
   it('returns true if both arguments are false', () => {
-    expect(nor(boolean(false), boolean(false))).toEqual(boolean(true))
+    expectWriter(
+      nor(boolean(false), boolean(false))
+    )(
+      boolean(true),
+      [[boolean(false), boolean(false)], 'joint denial complementation'],
+      [boolean(false), 'boolean complement']
+    )
   })
 
   it('returns the complement of the left operand if the right is false', () => {
-    expect(nor(variable('x'), boolean(false))).toEqual(not(variable('x')))
+    expectWriter(
+      nor(variable('x'), boolean(false))
+    )(
+      not(variable('x')),
+      [[variable('x'), boolean(false)], 'joint denial complementation'],
+      [variable('x'), 'complement']
+    )
   })
 
   it('returns the complement of the right operand if the left is false', () => {
-    expect(nor(boolean(false), variable('x'))).toEqual(not(variable('x')))
+    expectWriter(
+      nor(boolean(false), variable('x'))
+    )(
+      not(variable('x')),
+      [[boolean(false), variable('x')], 'joint denial complementation'],
+      [variable('x'), 'complement']
+    )
   })
 
   it('returns false if the right operand is true', () => {
-    expect(nor(variable('x'), boolean(true))).toEqual(boolean(false))
+    expectWriter(
+      nor(variable('x'), boolean(true))
+    )(
+      boolean(false),
+      [[variable('x'), boolean(true)], 'joint denial annihilator']
+    )
   })
 
   it('returns false if the left operand is true', () => {
-    expect(nor(boolean(true), variable('x'))).toEqual(boolean(false))
+    expectWriter(
+      nor(boolean(true), variable('x'))
+    )(
+      boolean(false),
+      [[boolean(true), variable('x')], 'joint denial annihilator']
+    )
   })
 
   it('returns the complement of the left operand if left is equivalent to right', () => {
-    expect(nor(variable('x'), variable('x'))).toEqual(not(variable('x')))
+    expectWriter(
+      nor(variable('x'), variable('x'))
+    )(
+      not(variable('x')),
+      [[variable('x'), variable('x')], 'joint denial complementation'],
+      [variable('x'), 'complement']
+    )
   })
 
   it('returns a conjunction of mutually complemented operands', () => {
-    expect(
+    expectWriter(
       nor(not(variable('x')), not(variable('y')))
-    ).toEqual(and(variable('x'), variable('y')))
+    )(
+      and(variable('x'), variable('y')),
+      [variable('x'), 'complement'],
+      [variable('y'), 'complement'],
+      [[not(variable('x')), not(variable('y'))], 'De Morgan'],
+      [[variable('x'), variable('y')], 'conjunction']
+    )
   })
 
   it('returns false if the right operand is the complement of the left', () => {
-    expect(nor(variable('x'), not(variable('x')))).toEqual(boolean(false))
+    expectWriter(
+      nor(variable('x'), not(variable('x')))
+    )(
+      boolean(false),
+      [variable('x'), 'complement'],
+      [[variable('x'), not(variable('x'))], 'joint denial annihilator']
+    )
   })
 
   it('returns false if the left operand is the complement of the right', () => {
-    expect(nor(not(variable('x')), variable('x'))).toEqual(boolean(false))
+    expectWriter(
+      nor(not(variable('x')), variable('x'))
+    )(
+      boolean(false),
+      [variable('x'), 'complement'],
+      [[not(variable('x')), variable('x')], 'joint denial annihilator']
+    )
   })
 
   it('returns a JointDenial on variable input', () => {
--- packages/calcula/src/functions/connectives/jointDenial.ts
+++ packages/calcula/src/functions/connectives/jointDenial.ts
@@ -5,8 +5,9 @@ import { Real, Complex, Boolean, boolean } from "../../primitives"
 import { BinaryNode, binary, when } from "../../closures/binary"
 import { deepEquals, isValue } from "../../utility/deepEquals"
 import { Connective } from './connective'
-import { not } from './complement'
+import { Complement, isComplement, not } from './complement'
 import { or } from './disjunction'
+import { and } from './conjunction'
 
 export type JointDenial = Connective<Species.nor>
 
@@ -17,6 +18,37 @@ export const [nor, isJointDenial, $nor] = binary<JointDenial, Boolean>(
   (l, r) => [not(or(unit(l), unit(r))), 'complex joint denial'],
   (l, r) => [not(or(unit(l), unit(r))), 'boolean joint denial']
 )(
+  when(
+    [_, isValue(boolean(true))],
+    [boolean(false), 'joint denial annihilator']
+  ),
+  when(
+    [isValue(boolean(true)), _],
+    [boolean(false), 'joint denial annihilator']
+  ),
+  when(
+    [_, isValue(boolean(false))],
+    (l, _r) => [not(unit(l)), 'joint denial complementation']
+  ),
+  when(
+    [isValue(boolean(false)), _],
+    (_l, r) => [not(unit(r)), 'joint denial complementation']
+  ),
+  when(
+    deepEquals, (l, _r) => [not(unit(l)) ,'joint denial complementation']
+  ),
+  when<Complement, Complement>(
+    (l, r) => isComplement(l) && isComplement(r),
+    (l, r) => [and(l.expression, r.expression), 'De Morgan']
+  ),
+  when(
+    (l, r) => isComplement(r) && deepEquals(l, r.value.expression),
+    [boolean(false), 'joint denial annihilator']
+  ),
+  when(
+    (l, r) => isComplement(l) && deepEquals(l.value.expression, r),
+    [boolean(false), 'joint denial annihilator']
+  )
   // method([_, bool(true)], bool(false)),
   // method([bool(true), _], bool(false)),
   // method([_, bool(false)], (l: Base, _r: Boolean) => not(l)),
