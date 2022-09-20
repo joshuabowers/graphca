1ae720ab66dc7cf4acd29a37cf37185bf4694a90
Joshua Bowers <joshua.bowers+code@gmail.com>
2022-09-20 14:21:08 -0700

    Moved much of the client code to frontend.
    Not currently re-contextualized. Will need edits.

--- apps/frontend/src/index.tsx
+++ apps/frontend/src/index.tsx
@@ -1,17 +1,29 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './app/App';
+import { store } from './app/store';
+import { Provider } from 'react-redux';
 import reportWebVitals from './reportWebVitals';
+import { ErrorBoundary } from './features/ErrorBoundary';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const renderApp = () =>
+  ReactDOM.render(
+    <React.StrictMode>
+      <Provider store={store}>
+        <ErrorBoundary>
+          <App />
+        </ErrorBoundary>
+      </Provider>
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+
+if(process.env.NODE_ENV !== 'production' && module.hot){
+  module.hot.accept('./app/App', renderApp);
+}
+
+renderApp();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
