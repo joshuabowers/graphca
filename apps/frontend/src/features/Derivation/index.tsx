5bfffb2d251eedf587522bcf54075df2f2541419
Joshua Bowers <joshua.bowers+code@gmail.com>
2022-09-27 16:35:03 -0700

    Minor tweaks
    1. Perhaps resolved a rendering issue in Edge wrt app full-pagedness.
    2. Added inputs to derivation logs. LIkely need to also log outputs
    of interstitial steps for the log information to be more reasonable.
    TODO: Need to resolve height issues in terminal wrt Derivations;
    added logging is throwing everything off. Possibly look into only
    rendering derivations when requested, rather than once. Would require
    adding in extra states to handle the transitions, or use of a library.

--- apps/frontend/src/features/Derivation/index.tsx
+++ apps/frontend/src/features/Derivation/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import { W, TreeNode } from '@bowers/calcula'
+import { W, TreeNode, isTreeNode } from '@bowers/calcula'
+import { Expression } from '../Expression'
 import styles from './Derivation.module.css'
 
 export interface DerivationProps {
@@ -7,12 +8,17 @@ export interface DerivationProps {
   show: boolean
 }
 
+const join = (elements: JSX.Element[]) =>
+  elements.reduce((p, c) => !p ? c : <>{p}, {c}</>)
+
 export const Derivation = (props: DerivationProps) => {
   return (
     <ol className={[styles.normal, props.show && styles.visible].join(' ')}>
       {
         props.for.log.map(({input, action}, i) => {
-          return <li key={i}>{action}</li>
+          const inputs = Array.isArray(input) ? input : [input]
+          const asExpressions = inputs.map(i => <Expression node={W.unit(i)}/>)
+          return <li key={i}>{join(asExpressions)}: {action}</li>
         })
       }
     </ol>
