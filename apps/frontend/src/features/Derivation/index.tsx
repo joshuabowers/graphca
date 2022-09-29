4c3cb31c3dce4a4acbd6d77a57aa66f7bfdcf4bf
Joshua Bowers <joshua.bowers+code@gmail.com>
2022-09-29 14:42:27 -0700

    Some notes on how derivations should look.

--- apps/frontend/src/features/Derivation/index.tsx
+++ apps/frontend/src/features/Derivation/index.tsx
@@ -11,6 +11,19 @@ export interface DerivationProps {
 const join = (elements: JSX.Element[]) =>
   elements.reduce((p, c) => !p ? c : <>{p}, {c}</>)
 
+// Expected derivation output:
+// Generates a grid, where each row represents a step in the derivation
+// Each row begins with a semantic counter element, followed by
+// Expression wrapped TreeNodes for the input and output. The output cell
+// should be left-aligned, with it's ::before having content='==='. The
+// input cell should be right-aligned. Final cell within a row is the
+// textual description of what is happening in that step.
+// The description cell should have ::before content of '--'
+// The Counter cell should be right-aligned, and preferably a different
+// color to the rest of the row.
+// E.g.:
+// 1) 1 + 2 === 3 -- Real addition
+
 export const Derivation = (props: DerivationProps) => {
   return (
     <ol className={[styles.normal, props.show && styles.visible].join(' ')}>
