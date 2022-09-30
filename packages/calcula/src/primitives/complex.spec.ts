4c758e20951f80b140b459fb1f1e9d4c2260d0ae
Joshua Bowers <joshua.bowers+code@gmail.com>
2022-09-30 16:37:58 -0700

    Fixed specs for complex, wrt inputs and output.

--- packages/calcula/src/primitives/complex.spec.ts
+++ packages/calcula/src/primitives/complex.spec.ts
@@ -15,32 +15,39 @@ describe('complex', () => {
   })
 
   it('returns a Writer<Complex> for a real input', () => {
+    const output = {
+      clade: Clades.primitive, genus: undefined, species: Species.complex, 
+      a: 5, b: 0
+    }
     expect(complex(real(5))).toEqual({
-      value: {
-        clade: Clades.primitive, genus: undefined, species: Species.complex, 
-        a: 5, b: 0
-      },
-      log: [{input: real(5).value, action: 'cast to complex'}]
+      value: output,
+      log: [{
+        inputs: [real(5).value], 
+        output,
+        action: 'cast to complex'
+      }]
     })
   })
 
   it('returns a Writer<Complex> for a complex input', () => {
+    const output = {
+      clade: Clades.primitive, genus: undefined, species: Species.complex, 
+      a: 1, b: 2
+    }
     expect(complex(complex([1, 2]))).toEqual({
-      value: {
-        clade: Clades.primitive, genus: undefined, species: Species.complex, 
-        a: 1, b: 2
-      },
-      log: [{input: complex([1, 2]).value, action: ''}]
+      value: output,
+      log: [{inputs: [complex([1, 2]).value], output, action: ''}]
     })
   })
 
   it('returns a Writer<Complex> for a boolean input', () => {
+    const output = {
+      clade: Clades.primitive, genus: undefined, species: Species.complex, 
+      a: 1, b: 0
+    }
     expect(complex(boolean(true))).toEqual({
-      value: {
-        clade: Clades.primitive, genus: undefined, species: Species.complex, 
-        a: 1, b: 0
-      },
-      log: [{input: boolean(true).value, action: 'cast to complex'}]
+      value: output,
+      log: [{inputs: [boolean(true).value], output, action: 'cast to complex'}]
     })
   })
 })
