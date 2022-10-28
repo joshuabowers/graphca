66e123b36afac61aef5ab6b318b99600e0327f3b
Joshua Bowers <joshua.bowers+code@gmail.com>
2022-10-27 18:05:41 -0700

    Updated combinatorics spec to latest behavior

--- packages/calcula/src/functions/combinatorics.spec.ts
+++ packages/calcula/src/functions/combinatorics.spec.ts
@@ -1,8 +1,20 @@
-import { expectCloseTo, expectWriter } from '../utility/expectations';
+import { unit } from '../monads/writer';
+import { expectCloseTo, expectWriterTreeNode } from '../utility/expectations';
 import { Clades, Genera, Species } from '../utility/tree';
 import { real, complex } from '../primitives';
 import { variable } from '../variable'
-import { Permutation, Combination, permute, combine } from "./combinatorics";
+import { permute, combine, $permute, $combine } from "./combinatorics";
+
+describe('$permute', () => {
+  it('generates a Permutation for two TreeNode inputs', () => {
+    expect(
+      $permute(unit(variable('n').value), unit(variable('r').value))[0]
+    ).toEqual({
+      clade: Clades.binary, genus: Genera.combinatorics, species: Species.permute,
+      left: unit(variable('n').value), right: unit(variable('r').value)
+    })
+  })
+})
 
 describe('permute', () => {
   it('calculates the permutation for real inputs', () => {
@@ -18,13 +30,25 @@ describe('permute', () => {
   })
 
   it('generates a Permutation for unbound sub-expressions', () => {
-    expectWriter(permute(variable('n'), variable('r')))(
-      {
-        clade: Clades.binary, genus: Genera.combinatorics, species: Species.permute,
-        left: variable('n'), right: variable('r')
-      } as Permutation,
-      [[variable('n').value, variable('r').value], 'permutation']
+    expectWriterTreeNode(
+      permute(variable('n'), variable('r')),
+      $permute(unit(variable('n').value), unit(variable('r').value))[0]
+    )(
+      ['n', 'n', 'given variable'],
+      ['r', 'r', 'given variable'],
+      ['P(n, r)', 'P(n,r)', 'permutation']
     )
+  })
+})
+
+describe('$combine', () => {
+  it('generates a Combination for two TreeNode inputs', () => {
+    expect(
+      $combine(unit(variable('n').value), unit(variable('r').value))[0]
+    ).toEqual({
+      clade: Clades.binary, genus: Genera.combinatorics, species: Species.combine,
+      left: unit(variable('n').value), right: unit(variable('r').value)
+    })
   })
 })
 
@@ -42,12 +66,13 @@ describe('combine', () => {
   })
 
   it('generates a Combination for unbound sub-expressions', () => {
-    expectWriter(combine(variable('n'), variable('r')))(
-      {
-        clade: Clades.binary, genus: Genera.combinatorics, species: Species.combine,
-        left: variable('n'), right: variable('r')
-      } as Combination,
-      [[variable('n').value, variable('r').value], 'combination']
+    expectWriterTreeNode(
+      combine(variable('n'), variable('r')),
+      $combine(unit(variable('n').value), unit(variable('r').value))[0]
+    )(
+      ['n', 'n', 'given variable'],
+      ['r', 'r', 'given variable'],
+      ['C(n, r)', 'C(n,r)', 'combination']
     )
   })
 })
