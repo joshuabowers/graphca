c5bf4077ee627f8e4b8e9c6d4a37f1917edb639f
Joshua Bowers <joshua.bowers+code@gmail.com>
2023-01-25 15:07:49 -0800

    Updated implication spec to current behavior
    Uses expectWriterTreeNode for most of its behavior.

--- packages/calcula/src/functions/connectives/implication.spec.ts
+++ packages/calcula/src/functions/connectives/implication.spec.ts
@@ -1,91 +1,165 @@
-import { expectWriter } from '../../utility/expectations'
+import { unit } from '../../monads/writer'
+import { expectWriterTreeNode } from '../../utility/expectations'
 import { Clades, Genera, Species } from '../../utility/tree'
-import { real, complex, boolean } from '../../primitives'
+import { boolean } from '../../primitives'
 import { variable } from '../../variable'
 import { not } from './complement'
-import { Implication, implies } from './implication'
+import { implies, $implies } from './implication'
+import { Unicode } from '../../Unicode'
+
+describe('$implies', () => {
+  it('generates a Implication for a pair of TreeNode inputs', () => {
+    expect(
+      $implies(unit(variable('x').value), unit(variable('y').value))[0]
+    ).toEqual({
+      clade: Clades.binary, genus: Genera.connective, species: Species.implies,
+      left: unit(variable('x').value), right: unit(variable('y').value)
+    })
+  })
+})
 
 describe('implies', () => {
   it('returns true when given two true things', () => {
-    expectWriter(
-      implies(boolean(true), boolean(true))
+    expectWriterTreeNode(
+      implies(boolean(true), boolean(true)),
+      boolean(true)
     )(
-      boolean(true),
-      [[boolean(true), boolean(true)], 'implicative identity']
+      ['true', 'true', 'given primitive'],
+      ['true', 'true', 'given primitive'],
+      [
+        `true ${Unicode.implies} true`,
+        'true',
+        'implicative identity'
+      ]
     )
   })
 
   it('returns true if the left argument is false', () => {
-    expectWriter(
-      implies(boolean(false), boolean(true))
+    expectWriterTreeNode(
+      implies(boolean(false), boolean(true)),
+      boolean(true)
     )(
-      boolean(true),
-      [[boolean(false), boolean(true)], 'implicative annihilator']
+      ['false', 'false', 'given primitive'],
+      ['true', 'true', 'given primitive'],
+      [
+        `false ${Unicode.implies} true`,
+        'true',
+        'implicative annihilator'
+      ],
+      ['true', 'true', 'given primitive']
     )
   })
 
   it('returns false if the right argument is false', () => {
-    expectWriter(
-      implies(boolean(true), boolean(false))
+    expectWriterTreeNode(
+      implies(boolean(true), boolean(false)),
+      boolean(false)
     )(
-      boolean(false),
-      [[boolean(true), boolean(false)], 'implicative identity']
+      ['true', 'true', 'given primitive'],
+      ['false', 'false', 'given primitive'],
+      [
+        `true ${Unicode.implies} false`,
+        'false',
+        'implicative identity'
+      ]
     )
   })
 
   it('returns true if both arguments are false', () => {
-    expectWriter(
-      implies(boolean(false), boolean(false))
+    expectWriterTreeNode(
+      implies(boolean(false), boolean(false)),
+      boolean(true)
     )(
-      boolean(true),
-      [[boolean(false), boolean(false)], 'implicative annihilator']
+      ['false', 'false', 'given primitive'],
+      ['false', 'false', 'given primitive'],
+      [
+        `false ${Unicode.implies} false`,
+        'true',
+        'implicative annihilator'
+      ],
+      ['true', 'true', 'given primitive']
     )
   })
 
   it('returns the right operand if the left is true', () => {
-    expectWriter(
-      implies(boolean(true), variable('x'))
+    expectWriterTreeNode(
+      implies(boolean(true), variable('x')),
+      variable('x')
     )(
-      variable('x'),
-      [[boolean(true), variable('x')], 'implicative identity']
+      ['true', 'true', 'given primitive'],
+      ['x', 'x', 'given variable'],
+      [
+        `true ${Unicode.implies} x`,
+        'x',
+        'implicative identity'
+      ]
     )
   })
 
   it('returns true if the right operand is true', () => {
-    expectWriter(
-      implies(variable('x'), boolean(true))
+    expectWriterTreeNode(
+      implies(variable('x'), boolean(true)),
+      boolean(true)
     )(
-      boolean(true),
-      [[variable('x'), boolean((true))], 'implicative annihilator']
+      ['x', 'x', 'given variable'],
+      ['true', 'true', 'given primitive'],
+      [
+        `x ${Unicode.implies} true`,
+        'true',
+        'implicative annihilator'
+      ],
+      ['true', 'true', 'given primitive']
     )
   })
 
   it('returns true if the left operand is false', () => {
-    expectWriter(
-      implies(boolean(false), variable('x'))
+    expectWriterTreeNode(
+      implies(boolean(false), variable('x')),
+      boolean(true)
     )(
-      boolean(true),
-      [[boolean(false), variable('x')], 'implicative annihilator']
+      ['false', 'false', 'given primitive'],
+      ['x', 'x', 'given variable'],
+      [
+        `false ${Unicode.implies} x`,
+        'true',
+        'implicative annihilator'
+      ],
+      ['true', 'true', 'given primitive']
     )
   })
 
   it('returns the complement of the left if the right is false', () => {
-    expectWriter(
-      implies(variable('x'), boolean(false))
+    expectWriterTreeNode(
+      implies(variable('x'), boolean(false)),
+      not(variable('x'))
     )(
-      not(variable('x')),
-      [[variable('x'), boolean(false)], 'implicative complementation'],
-      [variable('x'), 'complement']
+      ['x', 'x', 'given variable'],
+      ['false', 'false', 'given primitive'],
+      [
+        `x ${Unicode.implies} false`,
+        `${Unicode.not}(x)`,
+        'implicative complementation'
+      ],
+      [
+        `${Unicode.not}(x)`,
+        `${Unicode.not}(x)`,
+        'complement'
+      ]
     )
   })
   
   it('returns an Implication on variable input', () => {
-    expectWriter(implies(variable('x'), variable('y')))(
-      {
-        clade: Clades.binary, genus: Genera.connective, species: Species.implies,
-        left: variable('x'), right: variable('y')
-      } as Implication,
-      [[variable('x').value, variable('y').value], 'implication']
+    expectWriterTreeNode(
+      implies(variable('x'), variable('y')),
+      $implies(unit(variable('x').value), unit(variable('y').value))[0]
+    )(
+      ['x', 'x', 'given variable'],
+      ['y', 'y', 'given variable'],
+      [
+        `x ${Unicode.implies} y`,
+        `(x${Unicode.implies}y)`,
+        'implication'
+      ]
     )
   })
 })
