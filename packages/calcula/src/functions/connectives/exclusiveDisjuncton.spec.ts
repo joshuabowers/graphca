7402ba322455228fbc8dac28a144cbf51b68d190
Joshua Bowers <joshua.bowers+code@gmail.com>
2023-01-25 18:32:08 -0800

    Updated exclusive disjunction spec to standard
    Makes use of new expectsWriterTreeNode for history log checks.

--- packages/calcula/src/functions/connectives/exclusiveDisjuncton.spec.ts
+++ packages/calcula/src/functions/connectives/exclusiveDisjuncton.spec.ts
@@ -1,102 +1,188 @@
-import { expectWriter } from '../../utility/expectations'
+import { unit } from '../../monads/writer'
+import { expectWriterTreeNode } from '../../utility/expectations'
 import { Clades, Genera, Species } from '../../utility/tree'
-import { real, complex, boolean } from '../../primitives'
+import { boolean } from '../../primitives'
 import { variable } from '../../variable'
 import { not } from './complement'
-import { ExclusiveDisjunction, xor } from './exclusiveDisjunction'
+import { xor, $xor } from './exclusiveDisjunction'
+import { Unicode } from '../../Unicode'
+
+describe('$xor', () => {
+  it('generates a Exclusive Disjunction for a pair of TreeNode inputs', () => {
+    expect(
+      $xor(unit(variable('x').value), unit(variable('y').value))[0]
+    ).toEqual({
+      clade: Clades.binary, genus: Genera.connective, species: Species.xor,
+      left: unit(variable('x').value), right: unit(variable('y').value)
+    })
+  })
+})
 
 describe('xor', () => {
   it('returns false when given two true things', () => {
-    expectWriter(
-      xor(boolean(true), boolean(true))
+    expectWriterTreeNode(
+      xor(boolean(true), boolean(true)),
+      boolean(false)
     )(
-      boolean(false),
-      [[boolean(true), boolean(true)], 'exclusive disjunctive complementation'],
-      [boolean(true), 'boolean complement']
+      ['true', 'true', 'given primitive'],
+      ['true', 'true', 'given primitive'],
+      [
+        `true ${Unicode.xor} true`,
+        `${Unicode.not}(true)`,
+        'exclusive disjunctive complementation'
+      ],
+      [
+        `${Unicode.not}(true)`,
+        'false',
+        'boolean complement'
+      ],
+      ['false', 'false', 'given primitive']
     )
   })
 
   it('returns true if the left argument is false', () => {
-    expectWriter(
-      xor(boolean(false), boolean(true))
+    expectWriterTreeNode(
+      xor(boolean(false), boolean(true)),
+      boolean(true)
     )(
-      boolean(true),
-      [[boolean(false), boolean(true)], 'exclusive disjunctive identity']
+      ['false', 'false', 'given primitive'],
+      ['true', 'true', 'given primitive'],
+      [
+        `false ${Unicode.xor} true`,
+        'true',
+        'exclusive disjunctive identity'
+      ]
     )
   })
 
   it('returns true if the right argument is false', () => {
-    expectWriter(
-      xor(boolean(true), boolean(false))
+    expectWriterTreeNode(
+      xor(boolean(true), boolean(false)),
+      boolean(true)
     )(
-      boolean(true),
-      [[boolean(true), boolean(false)], 'exclusive disjunctive identity']
+      ['true', 'true', 'given primitive'],
+      ['false', 'false', 'given primitive'],
+      [
+        `true ${Unicode.xor} false`,
+        'true',
+        'exclusive disjunctive identity'
+      ]
     )
   })
 
   it('returns false if both arguments are false', () => {
-    expectWriter(
-      xor(boolean(false), boolean(false))
+    expectWriterTreeNode(
+      xor(boolean(false), boolean(false)),
+      boolean(false)
     )(
-      boolean(false),
-      [[boolean(false), boolean(false)], 'exclusive disjunctive identity']
+      ['false', 'false', 'given primitive'],
+      ['false', 'false', 'given primitive'],
+      [
+        `false ${Unicode.xor} false`,
+        'false',
+        'exclusive disjunctive identity'
+      ]
     )
   })
 
   it('returns the right operand if the left is false', () => {
-    expectWriter(
-      xor(boolean(false), variable('x'))
+    expectWriterTreeNode(
+      xor(boolean(false), variable('x')),
+      variable('x')
     )(
-      variable('x'),
-      [[boolean(false), variable('x')], 'exclusive disjunctive identity']
+      ['false', 'false', 'given primitive'],
+      ['x', 'x', 'given variable'],
+      [
+        `false ${Unicode.xor} x`,
+        'x',
+        'exclusive disjunctive identity'
+      ]
     )
   })
 
   it('returns the left operand if the right is false', () => {
-    expectWriter(
-      xor(variable('x'), boolean(false))
+    expectWriterTreeNode(
+      xor(variable('x'), boolean(false)),
+      variable('x')
     )(
-      variable('x'),
-      [[variable('x'), boolean(false)], 'exclusive disjunctive identity']
+      ['x', 'x', 'given variable'],
+      ['false', 'false', 'given primitive'],
+      [
+        `x ${Unicode.xor} false`,
+        'x',
+        'exclusive disjunctive identity'
+      ]
     )
   })
 
   it('returns the complement of the right if the left is true', () => {
-    expectWriter(
-      xor(boolean(true), variable('x'))
+    expectWriterTreeNode(
+      xor(boolean(true), variable('x')),
+      not(variable('x'))
     )(
-      not(variable('x')),
-      [[boolean(true), variable('x')], 'exclusive disjunctive complementation'],
-      [variable('x'), 'complement']
+      ['true', 'true', 'given primitive'],
+      ['x', 'x', 'given variable'],
+      [
+        `true ${Unicode.xor} x`,
+        `${Unicode.not}(x)`,
+        'exclusive disjunctive complementation'
+      ],
+      [
+        `${Unicode.not}(x)`,
+        `${Unicode.not}(x)`,
+        'complement'
+      ]
     )
   })
 
   it('returns the complement of the left if the right is true', () => {
-    expectWriter(
-      xor(variable('x'), boolean(true))
+    expectWriterTreeNode(
+      xor(variable('x'), boolean(true)),
+      not(variable('x'))
     )(
-      not(variable('x')),
-      [[variable('x'), boolean(true)], 'exclusive disjunctive complementation'],
-      [variable('x'), 'complement']
+      ['x', 'x', 'given variable'],
+      ['true', 'true', 'given primitive'],
+      [
+        `x ${Unicode.xor} true`,
+        `${Unicode.not}(x)`,
+        'exclusive disjunctive complementation'
+      ],
+      [
+        `${Unicode.not}(x)`,
+        `${Unicode.not}(x)`,
+        'complement'
+      ]
     )
   })
 
   it('returns false if the left and right operands are equal', () => {
-    expectWriter(
-      xor(variable('x'), variable('x'))
+    expectWriterTreeNode(
+      xor(variable('x'), variable('x')),
+      boolean(false)
     )(
-      boolean(false),
-      [[variable('x'), variable('x')], 'exclusive disjunctive annihilator']
+      ['x', 'x', 'given variable'],
+      ['x', 'x', 'given variable'],
+      [
+        `x ${Unicode.xor} x`,
+        'false',
+        'exclusive disjunctive annihilator'
+      ],
+      ['false', 'false', 'given primitive']
     )
   })
 
   it('returns a ExclusiveDisjunction on variable input', () => {
-    expectWriter(xor(variable('x'), variable('y')))(
-      {
-        clade: Clades.binary, genus: Genera.connective, species: Species.xor,
-        left: variable('x'), right: variable('y')
-      } as ExclusiveDisjunction,
-      [[variable('x').value, variable('y').value], 'exclusive disjunction']
+    expectWriterTreeNode(
+      xor(variable('x'), variable('y')),
+      $xor(unit(variable('x').value), unit(variable('y').value))[0]
+    )(
+      ['x', 'x', 'given variable'],
+      ['y', 'y', 'given variable'],
+      [
+        `x ${Unicode.xor} y`,
+        `(x${Unicode.xor}y)`,
+        'exclusive disjunction'
+      ]
     )
   })  
 })
