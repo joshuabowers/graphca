354eb4ada5103ee1b78d698c8e4039d53284f487
Joshua Bowers <joshua.bowers+code@gmail.com>
2023-02-15 14:38:59 -0800

    Terminal focus improvements; sticky controls
    Terminal.slice now attempts to keep track of a focused entry relevant
    to whichever operation was just conducted; Terminal will change its
    currentRef to reflect whichever element corresponds to the stored
    focus value. In such a way, the terminal will auto-scroll to likely
    desirable entries under various user inputs.
    
    EntryControls is now positioned as sticky within its parent context.

--- apps/frontend/src/features/Terminal/Terminal.slice.ts
+++ apps/frontend/src/features/Terminal/Terminal.slice.ts
@@ -10,12 +10,14 @@ export interface TerminalState {
   name: string;
   currentLine: string[];
   history: Array<TerminalEntryState>;
+  focus?: number
 }
 
 const initialState: TerminalState = {
   name: 'terminal',
   currentLine: [],
-  history: []
+  history: [],
+  focus: undefined
 }
 
 export const terminalSlice = createSlice({
@@ -24,10 +26,12 @@ export const terminalSlice = createSlice({
   reducers: {
     keyPress: (state, action: PayloadAction<string>) => {
       state.currentLine.push(action.payload);
+      state.focus = undefined
     },
 
     deleteLast: (state) => {
       state.currentLine.pop();
+      state.focus = undefined
     },
 
     calculate: (state) => {
@@ -37,16 +41,20 @@ export const terminalSlice = createSlice({
         enteredAt: Date.now(),
         showDerivation: false
       });
+      state.focus = state.history[state.history.length-1].enteredAt
       state.currentLine = [];
     },
 
     toggleDerivation: (state, action: PayloadAction<number>) => {
       const entry = state.history.find(item => item.enteredAt === action.payload)
       if(entry){ entry.showDerivation = !entry.showDerivation }
+      state.focus = entry?.enteredAt
     },
 
     forget: (state, action: PayloadAction<number>) => {
+      const next = state.history.find(item => item.enteredAt > action.payload)
       state.history = state.history.filter(item => item.enteredAt !== action.payload)
+      state.focus = next?.enteredAt
     }
   }
 })
--- apps/frontend/src/features/Terminal/index.tsx
+++ apps/frontend/src/features/Terminal/index.tsx
@@ -18,6 +18,7 @@ const getCurrentLine = createSelector(
   terminal => terminal.currentLine.join('')
 )
 const getHistory = createSelector(getTerminal, (terminal) => terminal.history)
+const getFocus = createSelector(getTerminal, (terminal) => terminal.focus)
 
 export const Terminal = (props: TerminalProps) => {
   const [scope] = useState(createScope()) // note: setScope could be provided here.
@@ -32,20 +33,12 @@ export const Terminal = (props: TerminalProps) => {
   const currentRef = useRef<HTMLLIElement>(null)
   const history = useAppSelector(getHistory);
   const currentLine = useAppSelector(getCurrentLine);
+  const focus = useAppSelector(getFocus)
   const parsings = useAppSelector(getParsings)
 
-  /**
-   * While the last entry in the parsings list is a good default for the
-   * entry to auto-scroll to/focus on, it would be good to allow the individual
-   * entries to signal a priority override: this would allow the entry to
-   * ensure its visibility upon altering the terminal in some fashion. The
-   * most likely use case for this is showing/hiding the derivation for the
-   * entry. This could be facilitated by having state be updated in the
-   * appropriate dispatched actions triggered in EventControls.
-   */
   useLayoutEffect(() => {
     currentRef.current?.scrollIntoView({behavior: 'smooth'})
-  }, [currentRef, history])
+  }, [currentRef, history, focus, currentLine])
 
   /**
    * Could be used to insert text into the app, bypassing the KeyPad...
@@ -57,14 +50,15 @@ export const Terminal = (props: TerminalProps) => {
       {
         history.map((item, key) =>
           <React.Fragment key={item.enteredAt}>
-            <span className={styles.reference}
-              ref={key+1 === parsings.length ? currentRef : undefined}
-            />
+            {
+              focus === item.enteredAt &&
+              <span ref={currentRef} className={styles.reference} />
+            }
             {parsings[key]}
           </React.Fragment>
         )
       }
-      <span ref={currentRef} className={styles.currentMarker} />
+      <span ref={!focus ? currentRef : undefined} className={styles.currentMarker} />
       <span className={styles.currentLine} onPaste={handlePaste}>{currentLine}<span className={styles.caret}>|</span></span>
     </div>
   )
